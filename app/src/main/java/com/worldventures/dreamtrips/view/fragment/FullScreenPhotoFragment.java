bd53815429cb57a8b08d5c407df107fface83870
man4ik <vfrfhjy44>
2015-01-24 15:19:14 +0200

    Add photo flag action with description and confirm

--- app/src/main/java/com/worldventures/dreamtrips/view/fragment/FullScreenPhotoFragment.java
+++ app/src/main/java/com/worldventures/dreamtrips/view/fragment/FullScreenPhotoFragment.java
@@ -4,17 +4,23 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.techery.spares.annotations.Layout;
 import com.worldventures.dreamtrips.R;
+import com.worldventures.dreamtrips.core.model.FlagContent;
 import com.worldventures.dreamtrips.core.model.Photo;
 import com.worldventures.dreamtrips.presentation.FullScreenPhotoFragmentPM;
 import com.worldventures.dreamtrips.utils.UniversalImageLoader;
+import com.worldventures.dreamtrips.view.util.TextWatcherAdapter;
 
 import javax.inject.Inject;
 
@@ -84,12 +90,58 @@ public class FullScreenPhotoFragment extends BaseFragment<FullScreenPhotoFragmen
     @OnClick(R.id.iv_flag)
     public void actionFlag() {
         PopupMenu popup = new PopupMenu(getActivity(), ivFlag);
-        popup.getMenuInflater().inflate(R.menu.menu_flag, popup.getMenu());
+        FlagContent[] values = FlagContent.values();
+        for (int i = 0; i < values.length; i++) {
+            FlagContent flagContent = values[i];
+            popup.getMenu().add(0, i, i, flagContent.getTitle());
+        }
         popup.setOnMenuItemClickListener(item -> {
-            getPresentationModel().flagAction(item.getTitle().toString());
+            getPresentationModel().showFlagAction(item.getItemId());
             return true;
         });
         popup.show();
+    }
+
+    public void showFlagConfirmDialog(String reason, String desc) {
+        String content = "WorldVentures personnel will be notified review this image for " + reason.toLowerCase() + ". Do you want continue?";
+        new MaterialDialog.Builder(getActivity())
+                .title("Confirm")
+                .content(content)
+                .positiveText("Send")
+                .negativeText("Cancel")
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        getPresentationModel().sendFlagAction(reason, desc);
+                    }
+                })
+                .show();
+    }
+
+    public void showFlagDescription(String reason) {
+        MaterialDialog dialog = new MaterialDialog.Builder(getActivity())
+                .title("Flag")
+                .customView(R.layout.dialog_flag_description)
+                .positiveText("Accept")
+                .negativeText("Cancel")
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        String desc = ((EditText) dialog.getCustomView()
+                                .findViewById(R.id.tv_description))
+                                .getText().toString();
+                        showFlagConfirmDialog(reason, desc);
+                    }
+                }).build();
+        dialog.show();
+        Button positiveButton = dialog.getActionButton(DialogAction.POSITIVE);
+        EditText etDesc = (EditText) dialog.getCustomView().findViewById(R.id.tv_description);
+        etDesc.addTextChangedListener(new TextWatcherAdapter() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                positiveButton.setEnabled(s.toString().trim().length() > 0);
+            }
+        });
     }
 
     @Override

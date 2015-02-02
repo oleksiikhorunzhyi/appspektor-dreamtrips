8a3c3679fff90b31f6ed32bb02a14606a4e27d52
edward <dieyoung>
2015-02-02 19:19:18 +0200

    instabag enable

--- app/src/main/java/com/worldventures/dreamtrips/core/initializer/InstabugInitializer.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/initializer/InstabugInitializer.java
@@ -18,9 +18,7 @@ public class InstabugInitializer implements AppInitializer {
     public void initialize(Injector injector) {
         injector.inject(this);
         Instabug.initialize(context)
-                .setCrashReportingEnabled(false)
                 .setAnnotationActivityClass(InstabugAnnotationActivity.class)
-                .setCrashReportingEnabled(false)
                 .setShowIntroDialog(true)
                 .setEnableOverflowMenuItem(true);
     }

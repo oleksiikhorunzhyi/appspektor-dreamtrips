445be586c7496e0fa9bc46eec709c84260e3c41a
man4ik <vfrfhjy44>
2015-03-26 12:39:38 +0200

    extract date time handler to DateTimeUtils

---
+++ app/src/main/java/com/worldventures/dreamtrips/core/utils/DateTimeUtils.java
@@ -0,0 +1,81 @@
+package com.worldventures.dreamtrips.core.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import timber.log.Timber;
+
+public class DateTimeUtils {
+    public static final String DATE_FORMAT = "MMM dd, yyyy";
+    public static final String TIME_FORMAT = "hh:mm a";
+
+
+    public static String convertDateToString(int year, int month, int day) {
+        return convertDateToString(year, month, day, DATE_FORMAT);
+    }
+
+    public static String convertDateToString(int year, int month, int day, String dateFormat) {
+        SimpleDateFormat sim = new SimpleDateFormat(dateFormat, Locale.getDefault());
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        Date date = calendar.getTime();
+        return sim.format(date);
+    }
+
+
+    public static String convertTimeToString(int h, int m) {
+        return convertTimeToString(h, m, TIME_FORMAT);
+    }
+
+    public static String convertTimeToString(int h, int m, String timeFormat) {
+        SimpleDateFormat sim = new SimpleDateFormat(timeFormat, Locale.getDefault());
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.HOUR, h);
+        calendar.set(Calendar.MINUTE, m);
+        Date date = calendar.getTime();
+        return sim.format(date);
+    }
+
+
+    public static Date mergeDateTime(Date date, Date time) {
+        Calendar calendarA = Calendar.getInstance();
+        calendarA.setTime(date);
+        Calendar calendarB = Calendar.getInstance();
+        calendarB.setTime(time);
+
+        calendarA.set(Calendar.HOUR_OF_DAY, calendarB.get(Calendar.HOUR_OF_DAY));
+        calendarA.set(Calendar.MINUTE, calendarB.get(Calendar.MINUTE));
+        calendarA.set(Calendar.SECOND, calendarB.get(Calendar.SECOND));
+        calendarA.set(Calendar.MILLISECOND, calendarB.get(Calendar.MILLISECOND));
+
+        return calendarA.getTime();
+    }
+
+    public static Date dateFromString(String date) {
+        return dateFromString(date, DATE_FORMAT);
+    }
+
+    public static Date timeFromString(String time) {
+        return dateFromString(time, TIME_FORMAT);
+    }
+
+    public static Date dateFromString(String date, String dateFormat) {
+        DateFormat result = new SimpleDateFormat(dateFormat, Locale.getDefault());
+        try {
+            return result.parse(date);
+        } catch (ParseException e) {
+            Timber.e(e, "");
+        }
+        return null;
+    }
+
+}
--- app/src/main/java/com/worldventures/dreamtrips/modules/tripsimages/presenter/CreatePhotoFragmentPM.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/tripsimages/presenter/CreatePhotoFragmentPM.java
@@ -5,65 +5,40 @@ import android.net.Uri;
 
 import com.techery.spares.module.Annotations.Global;
 import com.worldventures.dreamtrips.core.repository.SnappyRepository;
+import com.worldventures.dreamtrips.core.utils.DateTimeUtils;
 import com.worldventures.dreamtrips.core.utils.events.InsertNewImageUploadTaskEvent;
 import com.worldventures.dreamtrips.modules.common.presenter.Presenter;
 import com.worldventures.dreamtrips.modules.tripsimages.uploader.ImageUploadTask;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
 import de.greenrobot.event.EventBus;
-import timber.log.Timber;
 
 public class CreatePhotoFragmentPM extends Presenter<CreatePhotoFragmentPM.View> {
-    public static final String TIME = "time";
-    public static final String DATE = "date";
-    public static final String DATE_FORMAT = "MMM dd, yyyy";
-    public static final String TIME_FORMAT = "hh:mm a";
     @Inject
     @Global
     EventBus eventBus;
     @Inject
     SnappyRepository db;
-    private String date;
-    private String time;
 
     public CreatePhotoFragmentPM(View view) {
         super(view);
     }
 
     public void onDataSet(int year, int month, int day) {
-        SimpleDateFormat sim = new SimpleDateFormat(DATE_FORMAT);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.clear();
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        Date date = calendar.getTime();
-        String format = sim.format(date);
+        String format = DateTimeUtils.convertDateToString(year, month, day);
         view.setDate(format);
     }
 
     public void onTimeSet(int h, int m) {
-        SimpleDateFormat sim = new SimpleDateFormat(TIME_FORMAT);
-        Calendar calendar = Calendar.getInstance();
-        calendar.clear();
-        calendar.set(Calendar.HOUR, h);
-        calendar.set(Calendar.MINUTE, m);
-        Date date = calendar.getTime();
-        String format = sim.format(date);
+        String format = DateTimeUtils.convertTimeToString(h, m);
         view.setTime(format);
     }
-
 
     public void saveAction() {
         if (view.getImageUri().toString().isEmpty()) {
@@ -77,7 +52,9 @@ public class CreatePhotoFragmentPM extends Presenter<CreatePhotoFragmentPM.View>
             action.setLatitude(0);
             action.setLongitude(0);
             action.setLocationName(view.getLocation());
-            action.setShotAt(getParsedDateTime(view.getDate(), view.getTime()));
+            Date date = DateTimeUtils.dateFromString(view.getDate());
+            Date time = DateTimeUtils.timeFromString(view.getTime());
+            action.setShotAt(DateTimeUtils.mergeDateTime(date, time));
             action.setTaskId(UUID.randomUUID().toString());
             db.saveUploadImageTask(action);
 
@@ -95,36 +72,6 @@ public class CreatePhotoFragmentPM extends Presenter<CreatePhotoFragmentPM.View>
         return new ArrayList<>(Arrays.asList(trimed));
     }
 
-    public Date getParsedDateTime(String dateS, String timeS) {
-        DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-        DateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT);
-        Date date = null;
-        try {
-            date = dateFormat.parse(dateS);
-
-            Date time = timeFormat.parse(timeS);
-            return combineDateTime(date, time);
-        } catch (ParseException e) {
-            Timber.e(e, "");
-        }
-        return null;
-
-    }
-
-    private Date combineDateTime(Date date, Date time) {
-        Calendar calendarA = Calendar.getInstance();
-        calendarA.setTime(date);
-        Calendar calendarB = Calendar.getInstance();
-        calendarB.setTime(time);
-
-        calendarA.set(Calendar.HOUR_OF_DAY, calendarB.get(Calendar.HOUR_OF_DAY));
-        calendarA.set(Calendar.MINUTE, calendarB.get(Calendar.MINUTE));
-        calendarA.set(Calendar.SECOND, calendarB.get(Calendar.SECOND));
-        calendarA.set(Calendar.MILLISECOND, calendarB.get(Calendar.MILLISECOND));
-
-        Date result = calendarA.getTime();
-        return result;
-    }
 
     public interface View extends Presenter.View {
         void end();

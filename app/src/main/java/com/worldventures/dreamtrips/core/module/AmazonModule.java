316724e8c3c6df634ee148b42059728b94a400b9
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-05-19 13:32:02 +0300

    Increase amazon upload timeout to 90 secs

--- app/src/main/java/com/worldventures/dreamtrips/core/module/AmazonModule.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/module/AmazonModule.java
@@ -2,9 +2,11 @@ package com.worldventures.dreamtrips.core.module;
 
 import android.content.Context;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.s3.transfermanager.TransferManager;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.worldventures.dreamtrips.BuildConfig;
 
 import javax.inject.Singleton;
@@ -17,18 +19,23 @@ public class AmazonModule {
 
     @Provides
     public CognitoCachingCredentialsProvider provideCredProvider(Context context) {
-        return new CognitoCachingCredentialsProvider(
-                context,
+        return new CognitoCachingCredentialsProvider(context,
                 BuildConfig.AWS_ACCOUNT_ID,
                 BuildConfig.COGNITO_POOL_ID,
                 BuildConfig.COGNITO_ROLE_UNAUTH,
-                null,
-                Regions.US_EAST_1);
+                null, Regions.US_EAST_1
+        );
     }
 
     @Provides
     @Singleton
     public TransferManager provideTransferManager(CognitoCachingCredentialsProvider credentialsProvider) {
-        return new TransferManager(credentialsProvider);
+        int connectionTimeout = 90 * 1000; // 90 secs
+        //
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setConnectionTimeout(connectionTimeout);
+        clientConfiguration.setSocketTimeout(connectionTimeout);
+        //
+        return new TransferManager(new AmazonS3Client(credentialsProvider, clientConfiguration));
     }
 }

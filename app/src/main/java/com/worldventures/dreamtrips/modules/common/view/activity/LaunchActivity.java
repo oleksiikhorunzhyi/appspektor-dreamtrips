f4eb01702d3d60fabcc55f588eacb79d0efa4bc7
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-05-26 15:03:31 +0300

    Revert "Add failed samsung 4.2 appcopmat issue test"
    This reverts commit 235278d3f65623de08e46037486c400eeb04c757.

--- app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/LaunchActivity.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/LaunchActivity.java
@@ -1,31 +1,18 @@
 package com.worldventures.dreamtrips.modules.common.view.activity;
 
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.techery.spares.annotations.Layout;
 import com.worldventures.dreamtrips.R;
 import com.worldventures.dreamtrips.modules.common.presenter.LaunchActivityPresenter;
 
-import butterknife.InjectView;
-
 @Layout(R.layout.activity_launch)
 public class LaunchActivity extends ActivityWithPresenter<LaunchActivityPresenter> {
-
-    @InjectView(R.id.test_toolbar)
-    Toolbar toolbar;
 
     @Override
     protected LaunchActivityPresenter createPresentationModel(Bundle savedInstanceState) {
         return new LaunchActivityPresenter();
-    }
-
-    @Override
-    protected void afterCreateView(Bundle savedInstanceState) {
-        super.afterCreateView(savedInstanceState);
-        toolbar.inflateMenu(R.menu.menu_test);
-        setSupportActionBar(toolbar);
     }
 
     @Override

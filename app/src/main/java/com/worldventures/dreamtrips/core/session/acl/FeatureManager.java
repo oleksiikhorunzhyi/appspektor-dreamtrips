31c999b54c947b5657d865a6cff16d417f2fd939
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-07-06 13:40:21 +0300

    Add helpers to FeatureManager

--- app/src/main/java/com/worldventures/dreamtrips/core/session/acl/FeatureManager.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/session/acl/FeatureManager.java
@@ -17,4 +17,37 @@ public class FeatureManager {
         if (userSession == null || userSession.getFeatures() == null) return false;
         return Queryable.from(userSession.getFeatures()).any(f -> f.name.equals(name));
     }
+
+    public void checkIf(@Feature.FeatureName String name, OnFeatureChecked onFeatureChecked) {
+        onFeatureChecked.onChecked(available(name));
+    }
+
+    public void with(@Feature.FeatureName String name, OnFeatureAvailable onAvailable) {
+        with(name, onAvailable, null);
+    }
+
+    public void without(@Feature.FeatureName String name, OnFeatureMissing onMissing) {
+        with(name, null, onMissing);
+    }
+
+    public void with(@Feature.FeatureName String name, OnFeatureAvailable onAvailable, OnFeatureMissing onMissing) {
+        if (available(name)) {
+            if (onAvailable != null) onAvailable.onAvailable();
+        } else {
+            if (onMissing != null) onMissing.onMissing();
+        }
+    }
+
+    public interface OnFeatureChecked {
+        void onChecked(boolean isAvailable);
+    }
+
+    public interface OnFeatureAvailable {
+        void onAvailable();
+    }
+
+    public interface OnFeatureMissing {
+        void onMissing();
+    }
+
 }

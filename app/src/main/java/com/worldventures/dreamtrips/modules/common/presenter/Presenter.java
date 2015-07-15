d0a67d258fabff6206091318da3a602b27be0d2b
edward <palevomr@gmail.com>
2015-07-15 17:46:45 +0300

    Fix cancelled

--- app/src/main/java/com/worldventures/dreamtrips/modules/common/presenter/Presenter.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/common/presenter/Presenter.java
@@ -143,7 +143,7 @@ public class Presenter<VT extends Presenter.View> implements DreamSpiceManager.F
     @Override
     public void handleError(SpiceException error) {
         if (error != null && !TextUtils.isEmpty(error.getMessage())) {
-            if (!error.getMessage().contains("canceled")) //hotfix, as robospice doesn't mark spice exception
+            if (!error.getMessage().contains("cancelled")) //hotfix, as robospice doesn't mark spice exception
                 view.informUser(error.getMessage());
         } else {
             view.informUser(R.string.smth_went_wrong);

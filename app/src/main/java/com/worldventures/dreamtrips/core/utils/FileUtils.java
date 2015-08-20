b64043a343e90d5db25249e52848dad6bd7fffe1
edward <palevomr@gmail.com>
2015-08-20 18:36:50 +0300

    Delete image from media storegae

--- app/src/main/java/com/worldventures/dreamtrips/core/utils/FileUtils.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/utils/FileUtils.java
@@ -1,7 +1,11 @@
 package com.worldventures.dreamtrips.core.utils;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.MediaStore;
 
 import com.innahema.collections.query.queriables.Queryable;
 
@@ -41,8 +45,6 @@ public class FileUtils {
         if (file.isDirectory()) {
             cleanDirectory(context, file);
         } else {
-            context.getContentResolver().delete(Uri.parse(file.getAbsolutePath()), "", null);
-
             if (!file.exists()) {
                 throw new FileNotFoundException("File does not exist: " + file);
             }
@@ -52,6 +54,25 @@ public class FileUtils {
                         "Unable to delete file: " + file;
                 throw new IOException(message);
             }
+
+            // Set up the projection (we only need the ID)
+            String[] projection = {MediaStore.Images.Media._ID};
+
+            // Match on the file path
+            String selection = MediaStore.Images.Media.DATA + " = ?";
+            String[] selectionArgs = new String[]{file.getAbsolutePath()};
+
+            // Query for the ID of the media matching the file path
+            Uri queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            ContentResolver contentResolver = context.getContentResolver();
+            Cursor c = contentResolver.query(queryUri, projection, selection, selectionArgs, null);
+            if (c.moveToFirst()) {
+                // We found the ID. Deleting the item via the content provider will also remove the file
+                long id = c.getLong(c.getColumnIndexOrThrow(MediaStore.Images.Media._ID));
+                Uri deleteUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                contentResolver.delete(deleteUri, null, null);
+            }
+            c.close();
         }
     }
 }

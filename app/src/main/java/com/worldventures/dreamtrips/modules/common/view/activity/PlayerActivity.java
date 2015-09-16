077d85a52e688e73662e2a0d1e232552ec6370fa
edward <palevomr@gmail.com>
2015-09-16 14:23:19 +0300

    Upate ijk version to support old devices

--- app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/PlayerActivity.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/PlayerActivity.java
@@ -1,73 +1,66 @@
 package com.worldventures.dreamtrips.modules.common.view.activity;
 
-import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBar;
 
 import com.techery.spares.annotations.Layout;
 import com.worldventures.dreamtrips.R;
 
 import butterknife.InjectView;
-import tv.danmaku.ijk.media.widget.MediaController;
-import tv.danmaku.ijk.media.widget.VideoView;
+import timber.log.Timber;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+import tv.danmaku.ijk.media.widget.media.AndroidMediaController;
+import tv.danmaku.ijk.media.widget.media.IjkVideoView;
 
-/**
- * 1 on 04.02.15.
- */
+
 @Layout(R.layout.player_activity_simple)
 public class PlayerActivity extends BaseActivity {
 
     @InjectView(R.id.myVideo)
-    protected VideoView videoView;
+    protected IjkVideoView videoView;
+    protected AndroidMediaController mediaController;
 
-    private ProgressDialog pDialog;
-    private boolean paused = false;
+    private boolean mBackPressed;
 
     @Override
     protected void afterCreateView(Bundle savedInstanceState) {
         super.afterCreateView(savedInstanceState);
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage(getString(R.string.buffering));
-        pDialog.setCancelable(false);
-        pDialog.show();
-
         Uri uri = getIntent().getData();
-        try {
-            // Start the MediaController
-            MediaController mediacontroller = new MediaController(
-                    this);
-            mediacontroller.setAnchorView(videoView);
-            // Get the URL from String VideoURL
-            videoView.setMediaController(mediacontroller);
+
+        // init player
+        IjkMediaPlayer.loadLibrariesOnce(null);
+
+        mediaController = new AndroidMediaController(this, false);
+
+        videoView.setMediaController(mediaController);
+
+        if (uri != null) {
             videoView.setVideoURI(uri);
-
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-        }
-
-        videoView.requestFocus();
-        videoView.setOnPreparedListener((mp) -> {
-            pDialog.dismiss();
             videoView.start();
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (paused) {
-            paused = false;
-            videoView.resume();
+        } else {
+            Timber.e("Null Data Source\n");
+            finish();
         }
-
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        paused = true;
-        videoView.pause();
+    public void onBackPressed() {
+        mBackPressed = true;
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mBackPressed || !videoView.isBackgroundPlayEnabled()) {
+            videoView.stopPlayback();
+            videoView.release(true);
+            videoView.stopBackgroundPlay();
+        } else {
+            videoView.enterBackground();
+        }
     }
 
 }

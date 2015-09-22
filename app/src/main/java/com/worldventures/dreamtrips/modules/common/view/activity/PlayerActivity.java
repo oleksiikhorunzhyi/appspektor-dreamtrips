1905ff21b8a1544d027f5bf4a7ba8625d169bee3
edward <palevomr@gmail.com>
2015-09-22 21:42:42 +0300

    Retry video playing if error

--- app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/PlayerActivity.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/common/view/activity/PlayerActivity.java
@@ -3,11 +3,14 @@ package com.worldventures.dreamtrips.modules.common.view.activity;
 import android.app.Dialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.techery.spares.annotations.Layout;
 import com.worldventures.dreamtrips.R;
 
 import butterknife.InjectView;
+import butterknife.OnClick;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import timber.log.Timber;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
@@ -20,15 +23,18 @@ public class PlayerActivity extends BaseActivity {
 
     @InjectView(R.id.myVideo)
     protected IjkVideoView videoView;
+    @InjectView(R.id.retry)
+    protected TextView retry;
     protected AndroidMediaController mediaController;
 
     private boolean mBackPressed;
 
+    Uri uri;
+
     @Override
     protected void afterCreateView(Bundle savedInstanceState) {
         super.afterCreateView(savedInstanceState);
-        Uri uri = getIntent().getData();
-
+        uri = getIntent().getData();
         // init player
         IjkMediaPlayer.loadLibrariesOnce(null);
 
@@ -41,11 +47,22 @@ public class PlayerActivity extends BaseActivity {
                     .setTitleText(getString(R.string.player_error_header))
                     .setContentText(getString(R.string.player_error));
 
+            retry.setVisibility(View.VISIBLE);
             sweetAlertDialog.setOnCancelListener(dialog -> finish());
             sweetAlertDialog.show();
             return true;
         });
 
+        playVideo();
+    }
+
+    @OnClick(R.id.retry)
+    void onRetry() {
+        playVideo();
+    }
+
+    private void playVideo() {
+        retry.setVisibility(View.GONE);
         if (uri != null) {
             videoView.setVideoURI(uri);
             videoView.start();
@@ -53,6 +70,7 @@ public class PlayerActivity extends BaseActivity {
             Timber.e("Null Data Source\n");
             finish();
         }
+
     }
 
     @Override

d4771b5aa807985ed5f089cb5173aeb57d3d6860
devAnatolii <shadownshadow@yandex.ua>
2015-10-12 11:31:00 +0300

    fix disappearing account number (AccountFragment)

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/profile/adapters/IgnoreFirstExpandedItemAdapter.java
@@ -0,0 +1,29 @@
+package com.worldventures.dreamtrips.modules.profile.adapters;
+
+import android.content.Context;
+
+import com.techery.spares.module.Injector;
+import com.techery.spares.ui.view.cell.AbstractCell;
+import com.worldventures.dreamtrips.modules.bucketlist.view.adapter.IgnoreFirstItemAdapter;
+
+import javax.inject.Provider;
+
+public class IgnoreFirstExpandedItemAdapter extends IgnoreFirstItemAdapter{
+
+    private boolean firstExpanded;
+
+    public IgnoreFirstExpandedItemAdapter(Context context, Provider<Injector> injector) {
+        super(context, injector);
+        firstExpanded = false;
+    }
+
+    @Override
+    public void onBindViewHolder(AbstractCell cell, int position) {
+        super.onBindViewHolder(cell, position);
+        if (cell instanceof Expandable && position == 0){
+            Expandable expandableCell = ((Expandable) cell);
+            expandableCell.setExpanded(firstExpanded);
+            expandableCell.setListener(expanded -> firstExpanded = expanded);
+        }
+    }
+}

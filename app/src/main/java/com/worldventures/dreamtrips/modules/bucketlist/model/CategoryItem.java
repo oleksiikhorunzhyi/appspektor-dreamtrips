9937da82ef9714f15aee9dce041dce295373c20c
dan41k <kput4uk@gmail.com>
2015-10-20 17:53:54 +0300

    Fix feed crashes by adding Creator to BaseEntity's childs

--- app/src/main/java/com/worldventures/dreamtrips/modules/bucketlist/model/CategoryItem.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/bucketlist/model/CategoryItem.java
@@ -1,10 +1,16 @@
 package com.worldventures.dreamtrips.modules.bucketlist.model;
 
+import android.os.Parcel;
+
 import com.worldventures.dreamtrips.modules.common.model.BaseEntity;
 
 public class CategoryItem extends BaseEntity {
 
     private String name;
+
+    public CategoryItem() {
+
+    }
 
     public String getName() {
         return name;
@@ -14,4 +20,27 @@ public class CategoryItem extends BaseEntity {
     public String toString() {
         return name;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(this.name);
+    }
+
+    public CategoryItem(Parcel in) {
+        super(in);
+        this.name = in.readString();
+    }
+
+    public static final Creator<CategoryItem> CREATOR = new Creator<CategoryItem>() {
+        @Override
+        public CategoryItem createFromParcel(Parcel in) {
+            return new CategoryItem(in);
+        }
+
+        @Override
+        public CategoryItem[] newArray(int size) {
+            return new CategoryItem[size];
+        }
+    };
 }

03483dd78c60a96e258865e916dc983336b9c73c
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-10-23 21:41:51 +0300

    Fix JodaTime for Android with proper initializer

---
+++ app/src/main/java/com/worldventures/dreamtrips/core/initializer/JodaTimeInitializer.java
@@ -0,0 +1,22 @@
+package com.worldventures.dreamtrips.core.initializer;
+
+import android.content.Context;
+
+import com.techery.spares.application.AppInitializer;
+import com.techery.spares.module.Injector;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class JodaTimeInitializer implements AppInitializer {
+
+    private Context context;
+
+    public JodaTimeInitializer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void initialize(Injector injector) {
+        JodaTimeAndroid.init(context);
+    }
+}

43a98b099a093bb0bd0678d403001ae64dda7bc5
dan41k <kput4uk@gmail.com>
2015-10-30 15:18:36 +0200

    Fix likers list count

--- app/src/main/java/com/worldventures/dreamtrips/modules/feed/api/GetUsersLikedEntityQuery.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/feed/api/GetUsersLikedEntityQuery.java
@@ -11,10 +11,6 @@ public class GetUsersLikedEntityQuery extends Query<ArrayList<User>> {
     int page;
     int perPage;
 
-    public GetUsersLikedEntityQuery(String uid, int perPage) {
-        this(uid, 1, perPage);
-    }
-
     public GetUsersLikedEntityQuery(String uid, int page, int perPage) {
         super((Class<ArrayList<User>>) new ArrayList<User>().getClass());
         this.uid = uid;

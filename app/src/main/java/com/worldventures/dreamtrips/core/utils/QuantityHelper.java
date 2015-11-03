63987f216346f57edfa182c37b3cfabf6b50c119
dan41k <kput4uk@gmail.com>
2015-11-03 15:12:01 +0200

    Remove plurals and add QuantityHelper

---
+++ app/src/main/java/com/worldventures/dreamtrips/core/utils/QuantityHelper.java
@@ -0,0 +1,14 @@
+package com.worldventures.dreamtrips.core.utils;
+
+import android.support.annotation.StringRes;
+
+public class QuantityHelper {
+
+    public static @StringRes int chooseResource(int size, @StringRes int singleRes, @StringRes int multipleRes) {
+        return size > 1 ? multipleRes : singleRes;
+    }
+
+    public static @StringRes int chooseResource(int size, @StringRes int zeroRes, @StringRes int singleRes, @StringRes int multipleRes) {
+        return size == 0 ? zeroRes : chooseResource(size, singleRes, multipleRes);
+    }
+}

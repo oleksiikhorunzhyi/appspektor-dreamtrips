725087643eee9d417cf56d2c68987415301b7318
edward <palevomr@gmail.com>
2015-11-03 15:40:31 +0200

    Integrate PermissionDispatcher, use it in DtlStartFragment

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/fragment/DtlStartFragment.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/fragment/DtlStartFragment.java
@@ -4,14 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.techery.spares.annotations.Layout;
@@ -27,6 +24,10 @@ import com.worldventures.dreamtrips.modules.dtl.presenter.DtlStartPresenter;
 
 import javax.inject.Inject;
 
+import permissions.dispatcher.DeniedPermission;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+import permissions.dispatcher.ShowsRationale;
 import timber.log.Timber;
 
 /**
@@ -36,17 +37,16 @@ import timber.log.Timber;
  */
 @Layout(R.layout.fragment_dtl_start_empty)
 @MenuResource(R.menu.menu_mock)
+@RuntimePermissions
 public class DtlStartFragment extends BaseFragment<DtlStartPresenter> implements DtlStartPresenter.View {
 
     private static final int REQUEST_CHECK_SETTINGS = 1488;
-    // permission request codes need to be < 256
-    private static final int RC_HANDLE_LOCATION_PERM = 3;
 
     @Inject
     ActivityResultDelegate activityResultDelegate;
 
     @Override
-    public void onResume() {
+    public void onResume() {g
         super.onResume();
         activityResult(activityResultDelegate.getRequestCode(),
                 activityResultDelegate.getResultCode(), activityResultDelegate.getData());
@@ -63,27 +63,22 @@ public class DtlStartFragment extends BaseFragment<DtlStartPresenter> implements
 
     @Override
     public void checkPermissions() {
-        int rc = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
-        if (rc == PackageManager.PERMISSION_GRANTED) {
-            getPresenter().permissionGranted();
-        } else {
-            requestLocationPermission();
-        }
+        DtlStartFragmentPermissionsDispatcher.locationPermissionWithCheck(this);
     }
 
-    private void requestLocationPermission() {
-        final String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    void locationPermission() {
+        getPresenter().permissionGranted();
+    }
 
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(getActivity(), permissions, RC_HANDLE_LOCATION_PERM);
-        } else {
-            Snackbar.make(getView(), R.string.permission_location_rationale,
-                    Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.ok, view -> ActivityCompat.requestPermissions(getActivity(), permissions,
-                            RC_HANDLE_LOCATION_PERM))
-                    .show();
-        }
+    @ShowsRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+    void showRationaleForCamera() {
+        Toast.makeText(getActivity(), R.string.permission_location_rationale, Toast.LENGTH_SHORT).show();
+    }
+
+    @DeniedPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    void showDeniedForCamera() {
+        Toast.makeText(getActivity(), R.string.no_location_permission, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -112,21 +107,7 @@ public class DtlStartFragment extends BaseFragment<DtlStartPresenter> implements
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != RC_HANDLE_LOCATION_PERM) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
-        }
-
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            getPresenter().permissionGranted();
-            return;
-        }
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.app_name)
-                .setMessage(R.string.no_location_permission)
-                .setPositiveButton(R.string.ok, (dialog, id) -> getActivity().finish())
-                .show();
+        DtlStartFragmentPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     private NavigationConfig provideNavigationConfig(@Nullable Parcelable bundle) {

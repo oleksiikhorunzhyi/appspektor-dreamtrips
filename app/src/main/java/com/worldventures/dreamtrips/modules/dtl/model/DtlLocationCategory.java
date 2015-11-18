c526bf7d349d40978e15af912f2de58e36207288
edward <palevomr@gmail.com>
2015-11-18 15:50:26 +0200

    Remove custom adapter for locations

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/DtlLocationCategory.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/DtlLocationCategory.java
@@ -1,40 +1,8 @@
 package com.worldventures.dreamtrips.modules.dtl.model;
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
-
-import java.io.IOException;
-
 public enum DtlLocationCategory {
     CITY,
     METRO,
     STATE,
-    COUNTRY;
-
-    public final static TypeAdapter<DtlLocationCategory> adapter = new DtlLocationCategoryAdapter();
-
-    public static class DtlLocationCategoryAdapter extends TypeAdapter<DtlLocationCategory> {
-
-        @Override
-        public void write(JsonWriter out, DtlLocationCategory value) throws IOException {
-            if (value == null) {
-                out.nullValue();
-            } else {
-                out.value(value.name());
-            }
-        }
-
-        @Override
-        public DtlLocationCategory read(JsonReader reader) throws IOException {
-            if (reader.peek() == JsonToken.NULL) {
-                reader.nextNull();
-                return null;
-            } else {
-                DtlLocationCategory t = valueOf(reader.nextString());
-                return t != null ? t : CITY;
-            }
-        }
-    }
+    COUNTRY
 }

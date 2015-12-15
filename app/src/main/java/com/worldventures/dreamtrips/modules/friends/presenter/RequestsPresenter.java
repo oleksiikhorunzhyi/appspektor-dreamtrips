a1cd8c3dbf5b8bcfe87a8df4e7d2c29b2faba3fe
kput4uk@gmail.com <kput4uk@gmail.com>
2015-12-15 19:33:50 +0200

    Disable multiple request sending in RequestCells

--- app/src/main/java/com/worldventures/dreamtrips/modules/friends/presenter/RequestsPresenter.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/friends/presenter/RequestsPresenter.java
@@ -41,8 +41,6 @@ public class RequestsPresenter extends Presenter<RequestsPresenter.View> {
 
     List<Circle> circles;
 
-    private boolean deleteRequestLocked;
-
     @Override
     public void takeView(View view) {
         super.takeView(view);
@@ -122,7 +120,7 @@ public class RequestsPresenter extends Presenter<RequestsPresenter.View> {
                             circles.get(position).getId()),
                     object -> {
                         eventBus.post(new ReloadFriendListEvent());
-                        onSuccess(event.getPosition());
+                        onSuccess(event.getUser());
                         updateRequestsCount();
                     },
                     this::onError);
@@ -130,20 +128,17 @@ public class RequestsPresenter extends Presenter<RequestsPresenter.View> {
     }
 
     public void onEvent(CancelRequestEvent event) {
-        deleteRequest(event.getUser().getId(), event.getPosition());
+        deleteRequest(event.getUser(), event.getPosition());
     }
 
     public void onEvent(HideRequestEvent event) {
-        deleteRequest(event.getUser().getId(), event.getPosition());
+        deleteRequest(event.getUser(), event.getPosition());
     }
 
-    private void deleteRequest(int userId, int position) {
-        if (deleteRequestLocked) return;
-        //
-        deleteRequestLocked = true;
+    private void deleteRequest(User user, int position) {
         view.startLoading();
-        doRequest(new DeleteRequestCommand(userId),
-                object -> onSuccess(position),
+        doRequest(new DeleteRequestCommand(user.getId()),
+                object -> onSuccess(user),
                 this::onError);
     }
 
@@ -152,25 +147,20 @@ public class RequestsPresenter extends Presenter<RequestsPresenter.View> {
         doRequest(new ActOnRequestCommand(event.getUser().getId(),
                         ActOnRequestCommand.Action.REJECT.name()),
                 object -> {
-                    onSuccess(event.getPosition());
+                    onSuccess(event.getUser());
                     updateRequestsCount();
                 },
                 this::onError);
     }
 
-    private void onSuccess(int position) {
-        deleteRequestLocked = false;
+    private void onSuccess(User user) {
         if (view != null) {
             view.finishLoading();
-            if (position < view.getAdapter().getItemCount()) {
-                view.getAdapter().remove(position);
-                view.getAdapter().notifyItemRemoved(position);
-            }
+            view.getAdapter().remove(user);
         }
     }
 
     private void onError(SpiceException exception) {
-        deleteRequestLocked = false;
         if (view != null) {
             view.finishLoading();
             handleError(exception);
--- app/src/main/java/com/worldventures/dreamtrips/modules/friends/view/cell/RequestCell.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/friends/view/cell/RequestCell.java
@@ -26,6 +26,14 @@ public class RequestCell extends BaseUserCell {
     FrameLayout hide;
     @InjectView(R.id.cancel_button)
     FrameLayout cancel;
+    @InjectView(R.id.hide)
+    View btnHide;
+    @InjectView(R.id.cancel)
+    View btnCancel;
+    @InjectView(R.id.reject)
+    View btnReject;
+    @InjectView(R.id.accept)
+    View btnAccept;
     @InjectView(R.id.buttonContainer)
     ViewGroup container;
 
@@ -60,27 +68,40 @@ public class RequestCell extends BaseUserCell {
                 tvMutual.setVisibility(View.GONE);
                 break;
         }
+
+        enableButtons();
     }
 
     @OnClick(R.id.accept)
     void onAccept() {
+        btnAccept.setEnabled(false);
         getEventBus().post(new AcceptRequestEvent(getModelObject(), getAdapterPosition()));
     }
 
     @OnClick(R.id.reject)
     void onReject() {
+        btnReject.setEnabled(false);
         getEventBus().post(new RejectRequestEvent(getModelObject(), getAdapterPosition()));
         TrackingHelper.tapMyFriendsButtonFeed(TrackingHelper.ATTRIBUTE_REJECT_FRIEND_REQUEST);
     }
 
     @OnClick(R.id.hide)
     void onHide() {
+        btnHide.setEnabled(false);
         getEventBus().post(new HideRequestEvent(getModelObject(), getAdapterPosition()));
     }
 
     @OnClick(R.id.cancel)
     void onCancel() {
+        btnCancel.setEnabled(false);
         getEventBus().post(new CancelRequestEvent(getModelObject(), getAdapterPosition()));
         TrackingHelper.tapMyFriendsButtonFeed(TrackingHelper.ATTRIBUTE_CANCEL_FRIEND_REQUEST);
+    }
+
+    private void enableButtons() {
+        btnAccept.setEnabled(true);
+        btnReject.setEnabled(true);
+        btnHide.setEnabled(true);
+        btnCancel.setEnabled(true);
     }
 }

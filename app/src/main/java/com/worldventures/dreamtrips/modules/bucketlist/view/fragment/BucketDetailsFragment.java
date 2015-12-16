a029f2ca2844f47b706f7ca7a673eec0700ab189
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-12-16 21:55:12 +0200

    Fix bucket item details for dining (container was missing)

--- app/src/main/java/com/worldventures/dreamtrips/modules/bucketlist/view/fragment/BucketDetailsFragment.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/bucketlist/view/fragment/BucketDetailsFragment.java
@@ -168,12 +168,7 @@ public class BucketDetailsFragment<T extends BucketItemDetailsPresenter> extends
 
     @Override
     public void setDescription(String description) {
-        if (TextUtils.isEmpty(description)) {
-            textViewDescription.setVisibility(View.GONE);
-        } else {
-            textViewDescription.setVisibility(View.VISIBLE);
-            textViewDescription.setText(description);
-        }
+        setText(textViewDescription, description);
     }
 
     @Override
@@ -234,21 +229,22 @@ public class BucketDetailsFragment<T extends BucketItemDetailsPresenter> extends
 
     @Override
     public void setupDiningView(DiningItem diningItem) {
-        if (diningItem != null) {
-            setText(diningName, diningItem.getName());
-            setText(diningPriceRange, diningItem.getPriceRange());
-            setText(diningAddress, diningItem.getAddress());
-            setText(diningPhone, diningItem.getPhoneNumber());
-            setText(diningSite, diningItem.getUrl());
-            if (TextUtils.isEmpty(diningItem.getUrl()) && TextUtils.isEmpty(diningItem.getPhoneNumber())) {
-                diningDivider.setVisibility(View.GONE);
-            } else {
-                diningDivider.setVisibility(View.VISIBLE);
-            }
-        } else {
+        if (diningItem == null) {
             diningContainer.setVisibility(View.GONE);
+            return;
         }
-
+        //
+        diningContainer.setVisibility(View.VISIBLE);
+        setText(diningName, diningItem.getName());
+        setText(diningPriceRange, diningItem.getPriceRange());
+        setText(diningAddress, diningItem.getAddress());
+        setText(diningPhone, diningItem.getPhoneNumber());
+        setText(diningSite, diningItem.getUrl());
+        if (TextUtils.isEmpty(diningItem.getUrl()) && TextUtils.isEmpty(diningItem.getPhoneNumber())) {
+            diningDivider.setVisibility(View.GONE);
+        } else {
+            diningDivider.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -297,7 +293,7 @@ public class BucketDetailsFragment<T extends BucketItemDetailsPresenter> extends
         viewPagerBucketGallery.setAdapter(adapter);
         viewPagerBucketGallery.setCurrentItem(0);
         Queryable.from(photos).forEachR(photo ->
-                        adapter.add(new FragmentItem(TripImagePagerFragment.class, ""))
+                adapter.add(new FragmentItem(TripImagePagerFragment.class, ""))
         );
         adapter.notifyDataSetChanged();
         circleIndicator.setViewPager(viewPagerBucketGallery);

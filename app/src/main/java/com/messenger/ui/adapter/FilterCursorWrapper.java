ea79839cae34cdb0aa95eeea3c2fdec7352d0a02
Roman Minenok <roman.minenok@gmail.com>
2015-12-18 14:24:25 +0700

    Improve filter cursor wrapper formatting

--- app/src/main/java/com/messenger/ui/adapter/FilterCursorWrapper.java
+++ app/src/main/java/com/messenger/ui/adapter/FilterCursorWrapper.java
@@ -7,23 +7,22 @@ import android.database.CursorWrapper;
  * FilterCursorWrapper is for casses when running SQL query to get new cursor is not desirable.
  */
 public class FilterCursorWrapper extends CursorWrapper {
-    private String filter;
-    private int column;
     private int[] index;
-    private int count=0;
-    private int pos=0;
+    private int count;
+    private int pos;
 
-    public FilterCursorWrapper(Cursor cursor,String filter,int column) {
+    public FilterCursorWrapper(Cursor cursor, String filter, int column) {
         super(cursor);
-        this.filter = filter.toLowerCase();
-        this.column = column;
-        if (this.filter != "") {
+        filter = filter.toLowerCase();
+
+        if (filter != "") {
             this.count = super.getCount();
             this.index = new int[this.count];
-            for (int i=0;i<this.count;i++) {
+            for (int i = 0; i < this.count; i++) {
                 super.moveToPosition(i);
-                if (this.getString(this.column).toLowerCase().contains(this.filter))
+                if (this.getString(column).toLowerCase().contains(filter)) {
                     this.index[this.pos++] = i;
+                }
             }
             this.count = this.pos;
             this.pos = 0;
@@ -31,7 +30,7 @@ public class FilterCursorWrapper extends CursorWrapper {
         } else {
             this.count = super.getCount();
             this.index = new int[this.count];
-            for (int i=0;i<this.count;i++) {
+            for (int i=0; i < this.count; i++) {
                 this.index[i] = i;
             }
         }
@@ -39,17 +38,17 @@ public class FilterCursorWrapper extends CursorWrapper {
 
     @Override
     public boolean move(int offset) {
-        return this.moveToPosition(this.pos+offset);
+        return this.moveToPosition(this.pos + offset);
     }
 
     @Override
     public boolean moveToNext() {
-        return this.moveToPosition(this.pos+1);
+        return this.moveToPosition(this.pos + 1);
     }
 
     @Override
     public boolean moveToPrevious() {
-        return this.moveToPosition(this.pos-1);
+        return this.moveToPosition(this.pos - 1);
     }
 
     @Override
@@ -59,13 +58,14 @@ public class FilterCursorWrapper extends CursorWrapper {
 
     @Override
     public boolean moveToLast() {
-        return this.moveToPosition(this.count-1);
+        return this.moveToPosition(this.count - 1);
     }
 
     @Override
     public boolean moveToPosition(int position) {
-        if (position >= this.count || position < 0)
+        if (position >= this.count || position < 0) {
             return false;
+        }
         return super.moveToPosition(this.index[position]);
     }
 

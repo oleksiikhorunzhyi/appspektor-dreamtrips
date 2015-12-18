41b6852ea0df77090b045a97f55479fe8dc51d39
edward <palevomr@gmail.com>
2015-12-18 18:25:18 +0200

    Update UI for multi currency, Implement CurrencyDtEditText to reuse amount input with currency symbol

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/custom/CurrencyDTEditText.java
@@ -0,0 +1,35 @@
+package com.worldventures.dreamtrips.modules.dtl.view.custom;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.worldventures.dreamtrips.R;
+import com.worldventures.dreamtrips.modules.common.view.custom.DTEditText;
+
+public class CurrencyDTEditText extends DTEditText {
+
+    public CurrencyDTEditText(Context context) {
+        super(context);
+    }
+
+    public CurrencyDTEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CurrencyDTEditText(Context context, AttributeSet attrs, int style) {
+        super(context, attrs, style);
+    }
+
+    public void setCurrencySymbol(String symbol) {
+        TextDrawable drawable = TextDrawable.builder()
+                .beginConfig()
+                .fontSize(getResources().getDimensionPixelSize(R.dimen.font_normal))
+                .textColor(getResources().getColor(R.color.black))
+                .endConfig()
+                .buildRect(symbol, Color.TRANSPARENT);
+        setCompoundDrawablePadding(getResources().getDimensionPixelSize(R.dimen.spacing_small));
+        setCompoundDrawables(drawable, null, null, null);
+    }
+}

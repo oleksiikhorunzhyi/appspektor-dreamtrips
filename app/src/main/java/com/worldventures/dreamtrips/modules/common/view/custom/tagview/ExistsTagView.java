bab0fc5ea14ddbd6cec014f58d73dfd3217666b5
man4ik <mensutove@gmail.com>
2015-12-21 18:04:53 +0200

    Refactor tag view

--- app/src/main/java/com/worldventures/dreamtrips/modules/common/view/custom/tagview/ExistsTagView.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/common/view/custom/tagview/ExistsTagView.java
@@ -1,9 +1,5 @@
 package com.worldventures.dreamtrips.modules.common.view.custom.tagview;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.IntEvaluator;
-import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
@@ -23,9 +19,6 @@ import butterknife.OnClick;
 public class ExistsTagView extends TagView implements View.OnClickListener {
 
     private static final long HIDE_DELETE_BUTTON_DELAY = 2000;
-    private int padding;
-    private ValueAnimator anim = new ValueAnimator();
-    private static final long PADDING_ANIMATION_DURATION = 100;
 
     @InjectView(R.id.tagged_user_name)
     public TextView taggedUserName;
@@ -57,28 +50,14 @@ public class ExistsTagView extends TagView implements View.OnClickListener {
     protected void initialize() {
         LayoutInflater.from(getContext()).inflate(R.layout.layout_tag_view_exist, this, true);
         ButterKnife.inject(this);
-        padding = getResources().getDimensionPixelSize(R.dimen.tag_exist_text_padding);
         setClickable(true);
         setOnClickListener(this);
-
-        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                taggedUserName.setWidth(taggedUserName.getWidth()); //Override WRAP_CONTENT
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                    getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                } else {
-                    getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                }
-            }
-        });
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         removeCallbacks(hideDeleteBtnRunnable);
-        anim.removeAllListeners();
     }
 
     @Override
@@ -86,43 +65,16 @@ public class ExistsTagView extends TagView implements View.OnClickListener {
         tagListener.onTagClicked(photoTag.getUser().getId());
     }
 
-    public void showDeleteButton(){
-        if (taggedUserName.getPaddingLeft() != padding){
-            return;
-        }
-        anim.setIntValues(padding, 0);
-        anim.setEvaluator(new IntEvaluator());
-
-        anim.addUpdateListener(animation -> {
-            int val = (Integer) animation.getAnimatedValue();
-            taggedUserName.setPadding(val, 0, (padding - val) * 2, 0);
-        });
-        anim.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                btnDeleteTag.setVisibility(View.VISIBLE);
-                divider.setVisibility(View.VISIBLE);
-                postDelayed(hideDeleteBtnRunnable, HIDE_DELETE_BUTTON_DELAY);
-                anim.removeListener(this);
-            }
-        });
-        anim.setDuration(PADDING_ANIMATION_DURATION);
-        anim.start();
+    public void showDeleteButton() {
+        btnDeleteTag.setVisibility(View.VISIBLE);
+        divider.setVisibility(View.VISIBLE);
+        postDelayed(hideDeleteBtnRunnable, HIDE_DELETE_BUTTON_DELAY);
     }
 
-    private void hideDeleteButton(){
+    private void hideDeleteButton() {
         removeCallbacks(hideDeleteBtnRunnable);
-        btnDeleteTag.setVisibility(View.INVISIBLE);
-        divider.setVisibility(View.INVISIBLE);
-
-        anim.setIntValues(0, padding);
-        anim.setEvaluator(new IntEvaluator());
-        anim.addUpdateListener(animation -> {
-            int val = (Integer) animation.getAnimatedValue();
-            taggedUserName.setPadding(val, 0, (padding * 2) - val, 0);
-        });
-        anim.setDuration(PADDING_ANIMATION_DURATION);
-        anim.start();
+        btnDeleteTag.setVisibility(View.GONE);
+        divider.setVisibility(View.GONE);
     }
 
     @Override

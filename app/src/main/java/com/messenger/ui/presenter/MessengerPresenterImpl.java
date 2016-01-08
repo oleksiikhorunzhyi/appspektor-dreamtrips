771a983bedab203d5c74839b504e86804543e628
devAnatolii <shadownshadow@yandex.ua>
2016-01-08 15:39:29 +0200

    fix creating or extending group conversation without internet connection

--- app/src/main/java/com/messenger/ui/presenter/MessengerPresenterImpl.java
+++ app/src/main/java/com/messenger/ui/presenter/MessengerPresenterImpl.java
@@ -1,15 +1,21 @@
 package com.messenger.ui.presenter;
 
 
+import android.content.Context;
 import android.os.Parcelable;
+import android.widget.Toast;
 
+import com.messenger.synchmechanism.ConnectionStatus;
 import com.messenger.synchmechanism.MessengerConnector;
 import com.messenger.ui.view.MessengerScreen;
+import com.worldventures.dreamtrips.R;
 
 import java.util.concurrent.TimeUnit;
 
 public abstract class MessengerPresenterImpl<V extends MessengerScreen, S extends Parcelable>
         extends BaseViewStateMvpPresenter<V, S> implements MessengerPresenter<V, S> {
+
+    protected ConnectionStatus currentConnectivityStatus = ConnectionStatus.DISCONNECTED;
 
     @Override
     public void onAttachedToWindow() {
@@ -18,10 +24,20 @@ public abstract class MessengerPresenterImpl<V extends MessengerScreen, S extend
                 .compose(bindView())
                 .throttleLast(50, TimeUnit.MILLISECONDS)
                 .subscribe(connectionStatus -> {
+                    currentConnectivityStatus = connectionStatus;
                     if (isViewAttached()) {
                         getView().onConnectionChanged(connectionStatus);
                     }
                 });
+    }
+
+    protected boolean isConnectionPresent(){
+        return currentConnectivityStatus == ConnectionStatus.CONNECTED;
+    }
+
+    protected void showAbsentConnectionMessage(Context context){
+        Toast.makeText(context, R.string.warning_no_internet_connection,
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override

3b8697e5072849c04aa5a8dfdcfb81e82582fbb5
Den Drobiazko <den.d@techery.io>
2016-01-31 17:02:36 +0200

    DTL :: static instantiator-method for convenience

--- app/src/main/java/com/worldventures/dreamtrips/core/rx/IoToMainComposer.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/rx/IoToMainComposer.java
@@ -6,6 +6,10 @@ import rx.schedulers.Schedulers;
 
 public class IoToMainComposer<T> implements Observable.Transformer<T, T> {
 
+    public static <T> IoToMainComposer<T> get() {
+        return new IoToMainComposer<>();
+    }
+
     @Override
     public Observable<T> call(Observable<T> observable) {
         return observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());

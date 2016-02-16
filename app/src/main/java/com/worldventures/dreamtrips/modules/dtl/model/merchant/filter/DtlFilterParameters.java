fcc69638fd5023c2d22e59ef2c87d66e49a073c8
Alex Khorunzhyi <alex.kh@techery.io>
2016-02-16 14:11:51 +0200

    DTL :: Added default values to filter params;

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/filter/DtlFilterParameters.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/filter/DtlFilterParameters.java
@@ -10,19 +10,47 @@ import java.util.Collections;
 import java.util.List;
 
 @Value.Immutable
-@Value.Style(privateNoargConstructor = true)
+// TODO : current MAX_DISTANCE assumes miles - wrong
 public abstract class DtlFilterParameters {
 
     public static final int MIN_PRICE = 1;
     public static final int MAX_PRICE = 5;
     public static final int MAX_DISTANCE = 50;
-    // TODO : current MAX_DISTANCE assumes miles - wrong
 
-    public abstract int getMinPrice();
-    public abstract int getMaxPrice();
-    //
-    public abstract int getMaxDistance();
-    //
-    public abstract List<DtlMerchantAttribute> getSelectedAmenities();
+    /**
+     * Gets min price.
+     * @return the min price
+     */
+    @Value.Default
+    public int getMinPrice() {
+        return MIN_PRICE;
+    }
+
+    /**
+     * Gets max price.
+     * @return the max price
+     */
+    @Value.Default
+    public int getMaxPrice() {
+        return MAX_PRICE;
+    }
+
+    /**
+     * Gets max distance.
+     * @return the max distance
+     */
+    @Value.Default
+    public int getMaxDistance() {
+        return MAX_DISTANCE;
+    }
+
+    /**
+     * Gets selected amenities.
+     * @return the selected amenities
+     */
+    @Value.Default
+    public List<DtlMerchantAttribute> getSelectedAmenities() {
+        return Collections.emptyList();
+    }
 
 }

156f400fcc9d6bd7e9e033c6b082c9ba78c2e29d
edward <palevomr@gmail.com>
2016-02-23 15:12:30 +0200

    Move messageBody parsing logics to separate place, map non-deserialisable messages or messages that break contract as message invalid message

---
+++ app/src/main/java/com/messenger/delegate/MessageBodyParser.java
@@ -0,0 +1,56 @@
+package com.messenger.delegate;
+
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.messenger.messengerservers.model.AttachmentHolder;
+import com.messenger.messengerservers.model.MessageBody;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import java.util.List;
+
+import timber.log.Timber;
+
+public class MessageBodyParser {
+
+    private static final MessageBody UNSUPPORTED_MESSAGE_BODY = new MessageBody.Builder()
+            .text("This message is invalid")
+            .build();
+
+    private final Gson gson;
+
+    public MessageBodyParser(Gson gson) {
+        this.gson = gson;
+    }
+
+    public MessageBody parseMessageBody(String stanzaMessageBody) {
+        String messageBodyJson = StringEscapeUtils.unescapeXml(stanzaMessageBody);
+
+        MessageBody messageBody;
+
+        try {
+            messageBody = gson.fromJson(messageBodyJson, MessageBody.class);
+            if (!validateMessageBody(messageBody)) messageBody = UNSUPPORTED_MESSAGE_BODY;
+        } catch (JsonSyntaxException syntaxException) {
+            Timber.e(syntaxException, "Fail to parse message body");
+            messageBody = UNSUPPORTED_MESSAGE_BODY;
+        }
+
+        return messageBody;
+    }
+
+    private boolean validateMessageBody(MessageBody messageBody) {
+        List<AttachmentHolder> attachments = messageBody.getAttachments();
+        if (TextUtils.isEmpty(messageBody.getText()) &&
+                (attachments == null || attachments.isEmpty())) {
+            return false;
+        } else if (attachments != null && !attachments.isEmpty()) {
+            for (AttachmentHolder attachment : attachments) {
+                if (attachment == null) return false;
+            }
+        }
+        return true;
+    }
+}

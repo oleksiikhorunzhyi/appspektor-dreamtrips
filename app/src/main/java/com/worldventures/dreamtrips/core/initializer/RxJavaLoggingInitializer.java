dc4571d8e471e1e3430422af24d181c915906eda
Dmitry Sovgyr <dimdron@gmail.com>
2016-02-26 18:30:21 +0200

    Add custom debug hook for Rx

--- app/src/main/java/com/worldventures/dreamtrips/core/initializer/RxJavaLoggingInitializer.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/initializer/RxJavaLoggingInitializer.java
@@ -2,7 +2,7 @@ package com.worldventures.dreamtrips.core.initializer;
 
 import com.techery.spares.application.AppInitializer;
 import com.techery.spares.module.Injector;
-import com.worldventures.dreamtrips.core.rx.debug.TimberDebugHook;
+import com.worldventures.dreamtrips.core.utils.DTTimberDebugHook;
 
 import rx.plugins.RxJavaPlugins;
 
@@ -10,6 +10,6 @@ public class RxJavaLoggingInitializer implements AppInitializer {
 
     @Override
     public void initialize(Injector injector) {
-        RxJavaPlugins.getInstance().registerObservableExecutionHook(new TimberDebugHook());
+        RxJavaPlugins.getInstance().registerObservableExecutionHook(new DTTimberDebugHook());
     }
 }

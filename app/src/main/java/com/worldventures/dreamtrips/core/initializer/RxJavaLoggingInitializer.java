c241ff4f4c5ffcc53cd2e96e58b20129bb20c47a
Den Drobiazko <den.d@techery.io>
2016-03-02 15:35:35 +0200

    Common :: add BuildConfig flag to enable Rx debug hook on dev-builds only

--- app/src/main/java/com/worldventures/dreamtrips/core/initializer/RxJavaLoggingInitializer.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/initializer/RxJavaLoggingInitializer.java
@@ -2,6 +2,7 @@ package com.worldventures.dreamtrips.core.initializer;
 
 import com.techery.spares.application.AppInitializer;
 import com.techery.spares.module.Injector;
+import com.worldventures.dreamtrips.BuildConfig;
 import com.worldventures.dreamtrips.core.utils.DTTimberDebugHook;
 
 import rx.plugins.RxJavaPlugins;
@@ -10,6 +11,7 @@ public class RxJavaLoggingInitializer implements AppInitializer {
 
     @Override
     public void initialize(Injector injector) {
-        RxJavaPlugins.getInstance().registerObservableExecutionHook(new DTTimberDebugHook());
+        if (BuildConfig.RX_DEBUG_HOOK_ENABLED)
+            RxJavaPlugins.getInstance().registerObservableExecutionHook(new DTTimberDebugHook());
     }
 }

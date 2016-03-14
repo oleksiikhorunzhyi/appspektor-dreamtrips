c7e095ff0df2b2244a56753b410552d59ef22bfc
man4ik <mensutove@gmail.com>
2016-03-14 16:41:28 +0200

    Add lazy initialisation for uploadery API

--- app/src/main/java/com/worldventures/dreamtrips/core/api/DreamSpiceService.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/api/DreamSpiceService.java
@@ -8,11 +8,15 @@ import com.octo.android.robospice.persistence.CacheManager;
 import com.octo.android.robospice.persistence.exception.CacheCreationException;
 import com.octo.android.robospice.retrofit.RetrofitGsonSpiceService;
 import com.techery.spares.module.Injector;
+import com.techery.spares.module.qualifier.Global;
 import com.worldventures.dreamtrips.BuildConfig;
+import com.worldventures.dreamtrips.core.utils.events.AppConfigUpdatedEvent;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 import dagger.Lazy;
+import de.greenrobot.event.EventBus;
 import retrofit.converter.Converter;
 import retrofit.converter.GsonConverter;
 
@@ -25,10 +29,15 @@ public class DreamSpiceService extends RetrofitGsonSpiceService {
     protected DreamTripsApi dreamTripsApi;
 
     @Inject
+    protected Provider<UploaderyApi> uploaderyApiLazy;
     protected UploaderyApi uploaderyApi;
 
     @Inject
     protected Lazy<SharedServicesApi> sharedServicesApi;
+
+    @Global
+    @Inject
+    EventBus eventBus;
 
     @Inject
     protected ConfigApi configApi;
@@ -58,11 +67,23 @@ public class DreamSpiceService extends RetrofitGsonSpiceService {
         addRetrofitInterface(SharedServicesApi.class);
         addRetrofitInterface(ConfigApi.class);
         addRetrofitInterface(DtlApi.class);
+        uploaderyApi = uploaderyApiLazy.get(); //be careful. should be in UI thread.
+        eventBus.register(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        eventBus.unregister(this);
+        super.onDestroy();
     }
 
     @Override
     public CacheManager createCacheManager(Application application) throws CacheCreationException {
         return super.createCacheManager(application);
+    }
+
+    public void onEventMainThread(AppConfigUpdatedEvent event) {
+        uploaderyApi = uploaderyApiLazy.get();
     }
 
     @Override

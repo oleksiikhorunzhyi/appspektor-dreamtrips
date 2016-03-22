0802339114dcdb922c44f230119ee56537e8616b
dmmal <dmmal@ciklum.com>
2016-03-22 12:10:34 +0200

    Social :: Added crash info parameters to verify the problem

--- app/src/main/java/com/messenger/util/CrashlyticsTracker.java
+++ app/src/main/java/com/messenger/util/CrashlyticsTracker.java
@@ -1,10 +1,14 @@
 package com.messenger.util;
 
+import android.support.v4.util.ArrayMap;
+
 import com.crashlytics.android.Crashlytics;
 import com.worldventures.dreamtrips.core.api.error.DtApiException;
 import com.worldventures.dreamtrips.core.api.error.ErrorResponse;
 
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Set;
 
 import retrofit.RetrofitError;
 
@@ -13,7 +17,6 @@ public final class CrashlyticsTracker {
     public static void trackError(Throwable throwable) {
         Crashlytics.logException(throwable);
     }
-
 
     public static void trackError(RetrofitError throwable) {
         Crashlytics.log("Server error");
@@ -27,6 +30,14 @@ public final class CrashlyticsTracker {
 
     }
 
+    public static void trackErrorWithParams(Throwable throwable, ArrayMap<String, ?> paramsMap) {
+        Set<String> keys = paramsMap.keySet();
+        for(String key : keys) {
+            Crashlytics.log(key + " : " + paramsMap.get(key));
+        }
+
+        Crashlytics.logException(throwable);
+    }
 
     private static String getErrorMessageForCrashlytics(RetrofitError cause) {
         return String.format("%s throws api exception", cause.getResponse().getUrl());

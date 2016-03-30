75823f9130184d162f50e10d18de16d2232db90c
Den Drobiazko <den.d@techery.io>
2016-03-30 14:38:09 +0300

    DTL-flow :: add base ViewState class

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/presenter/DtlLocationsSearchPresenter.java
+++
@@ -1,4 +0,0 @@
-package com.worldventures.dreamtrips.modules.dtl_flow.presenter;
-
-public class DtlLocationsSearchPresenter {
-}
---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/presenter/DtlLocationsSearchPresenterImpl.java
@@ -0,0 +1,7 @@
+package com.worldventures.dreamtrips.modules.dtl_flow.presenter;
+
+import com.worldventures.dreamtrips.modules.dtl_flow.screen.DtlLocationsSearchScreen;
+
+public interface DtlLocationsSearchPresenterImpl extends FlowPresenterImpl<DtlLocationsSearchScreen,>
+        implements  {
+}

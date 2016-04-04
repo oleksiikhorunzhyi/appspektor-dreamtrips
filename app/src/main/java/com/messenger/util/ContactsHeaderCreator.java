0615609d164c93b4f1a4af791c17cba7a1d9af71
Roman Minenok <roman.minenok@gmail.com>
2016-04-04 13:49:13 +0700

    Refactor ContactsHeaderCreator for new chat members screens

--- app/src/main/java/com/messenger/util/ContactsHeaderCreator.java
+++ app/src/main/java/com/messenger/util/ContactsHeaderCreator.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
@@ -22,45 +23,68 @@ public class ContactsHeaderCreator {
         this.context = context;
     }
 
-    public ContactsHeaderInfo createHeader(Collection<DataUser> contacts, String searchQuery) {
+    public ContactsHeaderInfo createHeader(Collection<DataUser> users, String searchQuery) {
+        String selectedContactsFormattedCount = getSelectedContactsFormattedCount(users);
+
+        String userNamesComaSeparated = Queryable.from(users).map(DataUser::getName)
+                .fold("", (list, username) -> list + username + ", ");
+        Spanned userNamesSpanned = getSpannedUserNames(users, userNamesComaSeparated);
+        Spanned searchQuerySpanned = getSpannedSearchQuery(searchQuery);
+        Spanned userNamesWithSearchQuerySpanned = (Spanned) TextUtils.concat(userNamesSpanned, searchQuerySpanned);
+
+        return new ContactsHeaderInfo(selectedContactsFormattedCount,
+                userNamesComaSeparated, userNamesWithSearchQuerySpanned);
+    }
+
+    /**
+     * Get first part of the selected contacts edit text message
+     * @param contacts
+     * @return formatted count of selected contacts, e.g. Add(3):
+     */
+    private String getSelectedContactsFormattedCount(Collection<DataUser> contacts) {
         String selectedContactsFormattedCount;
         if (contacts.isEmpty()) {
             selectedContactsFormattedCount = context.getString(R.string.new_chat_chosen_contacts_header_empty);
         } else {
             String addString = context.getString(R.string.new_chat_chosen_contacts_header_contacts_list_start_value);
-            selectedContactsFormattedCount = String.format("%s: (%d)", addString, contacts.size());
+            selectedContactsFormattedCount = String.format("%s (%d):", addString, contacts.size());
         }
-
-        String[] userNames = Queryable.from(contacts).map(DataUser::getName).toArray(String.class);
-        String contactsList = TextUtils.join(", ", userNames);
-        if (userNames.length > 0) contactsList += ", "; // add coma even to latest user
-        String contactsListWithSearchQuery = contactsList;
-        if (!TextUtils.isEmpty(searchQuery)) contactsListWithSearchQuery += searchQuery;
-        SpannableString contactsListSpannableString = new SpannableString(contactsListWithSearchQuery);
-        int previousUsernameIndex = 0;
-        for (String userName : userNames) {
-            int userNameIndexStart = contactsListWithSearchQuery.indexOf(userName, previousUsernameIndex);
-            int userNameIndexEnd = userNameIndexStart + userName.length();
-            previousUsernameIndex = userNameIndexEnd;
-            assignUnderlinedSpan(contactsListSpannableString, userNameIndexStart, userNameIndexEnd);
-            int coloredSpanEnd = userNameIndexEnd + 1; // +1 for coma
-            assignBlueSpan(contactsListSpannableString, userNameIndexStart, coloredSpanEnd);
-        }
-
-        if (!TextUtils.isEmpty(searchQuery)) {
-            int searchQuerySpanStartIndex = contactsListWithSearchQuery.indexOf(searchQuery, previousUsernameIndex);
-            int searchQuerySpanEnd = searchQuerySpanStartIndex + searchQuery.length();
-            assignBlueSpan(contactsListSpannableString, searchQuerySpanStartIndex, searchQuerySpanEnd);
-        }
-
-        return new ContactsHeaderInfo(selectedContactsFormattedCount, contactsList, contactsListSpannableString);
+        return selectedContactsFormattedCount;
     }
 
-    private void assignUnderlinedSpan(SpannableString spannableString, int start, int end) {
+    private Spanned getSpannedUserNames(Collection<DataUser> users, String usernamesComaSeparated) {
+        Spanned userNamesComaSeparatedSpanned =  new SpannableString(usernamesComaSeparated);
+        // save previous found username index each time to handle
+        // the case with the same usernames going right after each other in the list
+        int previousProcessedUserNameIndex = 0;
+        for (DataUser user : users) {
+            String userName = user.getName();
+            int userNameIndexStart = usernamesComaSeparated.indexOf(userName, previousProcessedUserNameIndex);
+            int userNameIndexEnd = userNameIndexStart + userName.length();
+
+            assignUnderlinedSpan((Spannable) userNamesComaSeparatedSpanned, userNameIndexStart, userNameIndexEnd);
+            // assign blue span to the coma after username also
+            assignBlueSpan((Spannable) userNamesComaSeparatedSpanned, userNameIndexStart, userNameIndexEnd + 1);
+
+            previousProcessedUserNameIndex = userNameIndexEnd;
+        }
+        return userNamesComaSeparatedSpanned;
+    }
+
+    private Spanned getSpannedSearchQuery(CharSequence searchQuery) {
+        if (TextUtils.isEmpty(searchQuery)) {
+            return new SpannableString("");
+        }
+        SpannableString searchQuerySpanned = new SpannableString(searchQuery);
+        assignBlueSpan(searchQuerySpanned, 0, searchQuery.length());
+        return searchQuerySpanned;
+    }
+
+    private void assignUnderlinedSpan(Spannable spannableString, int start, int end) {
         spannableString.setSpan(new UnderlineSpan(), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 
-    private void assignBlueSpan(SpannableString spannableString, int start, int end) {
+    private void assignBlueSpan(Spannable spannableString, int start, int end) {
         int spannableColor = ContextCompat.getColor(context, R.color.contact_list_header_selected_contacts);
         spannableString.setSpan(new ForegroundColorSpan(spannableColor), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
@@ -68,10 +92,10 @@ public class ContactsHeaderCreator {
     public static class ContactsHeaderInfo {
         String selectedContactsFormattedCount;
         String contactsList;
-        Spannable contactsListWithSearchQuery;
+        Spanned contactsListWithSearchQuery;
 
         public ContactsHeaderInfo(String selectedContactsFormattedCount, String contactsList,
-                                  Spannable contactsListWithSearchQuery) {
+                                  Spanned contactsListWithSearchQuery) {
             this.selectedContactsFormattedCount = selectedContactsFormattedCount;
             this.contactsList = contactsList;
             this.contactsListWithSearchQuery = contactsListWithSearchQuery;
@@ -85,7 +109,7 @@ public class ContactsHeaderCreator {
             return contactsList;
         }
 
-        public Spannable getContactsListWithSearchQuery() {
+        public Spanned getContactsListWithSearchQuery() {
             return contactsListWithSearchQuery;
         }
     }

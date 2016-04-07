c15a0a4c3164634da321927a019aa7658f5e50b5
Aleksey Malevaniy <a.malevaniy@gmail.com>
2016-04-07 16:01:55 +0300

    - fix image resizing for dtl data inflater

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/helper/inflater/DtlMerchantSingleImageDataInflater.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/helper/inflater/DtlMerchantSingleImageDataInflater.java
@@ -1,10 +1,12 @@
 package com.worldventures.dreamtrips.modules.dtl.helper.inflater;
 
 import android.net.Uri;
+import android.view.View;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.innahema.collections.query.queriables.Queryable;
 import com.worldventures.dreamtrips.R;
+import com.worldventures.dreamtrips.core.utils.GraphicUtils;
 import com.worldventures.dreamtrips.modules.dtl.helper.DtlMerchantHelper;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.DtlMerchant;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.DtlMerchantMedia;
@@ -34,6 +36,16 @@ public class DtlMerchantSingleImageDataInflater extends DtlMerchantCommonDataInf
             return;
         }
         //
-        cover.setImageURI(Uri.parse(media.getImagePath()));
+        cover.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (v.getWidth() == 0) return;
+                cover.removeOnLayoutChangeListener(this);
+                cover.setController(GraphicUtils.provideFrescoResizingController(
+                        Uri.parse(media.getImagePath()), cover.getController(),
+                        cover.getWidth(), cover.getHeight())
+                );
+            }
+        });
     }
 }

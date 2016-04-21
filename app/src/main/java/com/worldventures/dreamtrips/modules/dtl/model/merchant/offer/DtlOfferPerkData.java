9dd751f1f9d92e892e2409cf2fa218410fb6cf17
Alex Khorunzhyi <alex.kh@techery.io>
2016-04-21 15:13:55 +0300

    DTL :: change perks mapping

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/offer/DtlOfferPerkData.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/offer/DtlOfferPerkData.java
@@ -1,10 +1,47 @@
 package com.worldventures.dreamtrips.modules.dtl.model.merchant.offer;
 
+import com.worldventures.dreamtrips.modules.dtl.model.merchant.operational_hour.OperationDay;
+
+import java.util.Date;
+import java.util.List;
+
 public class DtlOfferPerkData extends DtlOfferData {
 
     private String description;
+    private String disclaimer;
+
+    private Date startDate;
+    private Date endDate;
+
+    private List<OperationDay> operationDays;
+    private List<DtlOfferMedia> images;
 
     public String getDescription() {
         return description;
+    }
+
+    public String getDisclaimer() {
+        return disclaimer;
+    }
+
+    public Date getStartDdate() {
+        return startDate;
+    }
+
+    public Date getEndDdate() {
+        return endDate;
+    }
+
+    public List<DtlOfferMedia> getImages() {
+        return images;
+    }
+
+    public List<OperationDay> getOperationDays() {
+        return operationDays;
+    }
+
+    @Override
+    public String getType() {
+        return Offer.PERKS;
     }
 }

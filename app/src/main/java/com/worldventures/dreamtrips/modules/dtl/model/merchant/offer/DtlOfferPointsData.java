5755207f993a5208ae5cc956b88a803adf83a2ae
Alex Khorunzhyi <alex.kh@techery.io>
2016-04-25 00:44:32 +0300

    DTL change offers base implementation

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/offer/DtlOfferPointsData.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/offer/DtlOfferPointsData.java
@@ -6,11 +6,17 @@ public class DtlOfferPointsData extends DtlOfferData {
 
     List<DtlCurrency> currencies;
 
+
     public List<DtlCurrency> getCurrencies() {
         return currencies;
     }
 
     public void setCurrencies(List<DtlCurrency> currencies) {
         this.currencies = currencies;
+    }
+
+    @Override
+    public String getType() {
+        return Offer.POINT_REWARD;
     }
 }

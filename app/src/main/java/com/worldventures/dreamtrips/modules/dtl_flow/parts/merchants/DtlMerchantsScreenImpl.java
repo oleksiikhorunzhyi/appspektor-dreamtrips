f75bc3d38f639d0c3e4d9334f0b5b1b5ca28f7a1
Alex Khorunzhyi <alex.kh@techery.io>
2016-04-25 00:46:54 +0300

    DTL :: handle perk/point clicks

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/merchants/DtlMerchantsScreenImpl.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/merchants/DtlMerchantsScreenImpl.java
@@ -4,17 +4,14 @@ import android.content.Context;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
 import com.jakewharton.rxbinding.widget.RxCompoundButton;
-import com.techery.spares.adapter.BaseDelegateAdapter;
-import com.techery.spares.adapter.expandable.BaseExpandableAdapter;
+import com.techery.spares.adapter.expandable.BaseExpandableDelegateAdapter;
 import com.techery.spares.adapter.expandable.ExpandableLayoutManager;
 import com.techery.spares.module.Injector;
 import com.techery.spares.module.qualifier.ForActivity;
@@ -29,11 +26,13 @@ import com.worldventures.dreamtrips.modules.common.view.custom.EmptyRecyclerView
 import com.worldventures.dreamtrips.modules.dtl.helper.SearchViewHelper;
 import com.worldventures.dreamtrips.modules.dtl.model.location.DtlLocation;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.DtlMerchant;
-import com.worldventures.dreamtrips.modules.dtl.model.merchant.offer.DtlOffer;
-import com.worldventures.dreamtrips.modules.dtl.view.cell.DtlMerchantCellNew;
+import com.worldventures.dreamtrips.modules.dtl.model.merchant.offer.DtlOfferPerkData;
+import com.worldventures.dreamtrips.modules.dtl.model.merchant.offer.DtlOfferPointsData;
 import com.worldventures.dreamtrips.modules.dtl.view.cell.DtlMerchantExpandableCell;
-import com.worldventures.dreamtrips.modules.dtl.view.cell.DtlOfferCell;
+import com.worldventures.dreamtrips.modules.dtl.view.cell.DtlPerkCell;
+import com.worldventures.dreamtrips.modules.dtl.view.cell.DtlPointsCell;
 import com.worldventures.dreamtrips.modules.dtl_flow.DtlLayout;
+import com.worldventures.dreamtrips.modules.membership.view.util.DividerItemDecoration;
 
 import java.util.List;
 
@@ -66,7 +65,7 @@ public class DtlMerchantsScreenImpl extends DtlLayout<DtlMerchantsScreen, DtlMer
     @InjectView(R.id.merchant_holder_offers)
     protected TextView emptyTextView;
     //
-    BaseExpandableAdapter<DtlMerchant> baseDelegateAdapter;
+    BaseExpandableDelegateAdapter baseDelegateAdapter;
     SearchViewHelper searchViewHelper;
     //
     SelectionManager selectionManager;
@@ -77,13 +76,17 @@ public class DtlMerchantsScreenImpl extends DtlLayout<DtlMerchantsScreen, DtlMer
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+
         recyclerView.setLayoutManager(new ExpandableLayoutManager(getActivity()));
         //
-        baseDelegateAdapter = new BaseExpandableAdapter<>(getActivity(), injector);
+        baseDelegateAdapter = new BaseExpandableDelegateAdapter(getActivity(), injector);
         baseDelegateAdapter.registerCell(DtlMerchant.class, DtlMerchantExpandableCell.class);
-        baseDelegateAdapter.registerCell(DtlOffer.class, DtlOfferCell.class);
+        baseDelegateAdapter.registerCell(DtlOfferPerkData.class, DtlPerkCell.class);
+        baseDelegateAdapter.registerCell(DtlOfferPointsData.class, DtlPointsCell.class);
 
-        //baseDelegateAdapter.registerDelegate(DtlMerchant.class, this);
+        baseDelegateAdapter.registerDelegate(DtlMerchant.class, this);
+        baseDelegateAdapter.registerDelegate(DtlOfferPerkData.class, new PerkDelegate());
+        baseDelegateAdapter.registerDelegate(DtlOfferPointsData.class, new PointsDelegate());
         //
         selectionManager = new SingleSelectionManager(recyclerView);
         selectionManager.setEnabled(isTabletLandscape());
@@ -216,6 +219,20 @@ public class DtlMerchantsScreenImpl extends DtlLayout<DtlMerchantsScreen, DtlMer
         alertDialog.setCancelable(false);
         alertDialog.show();
         return true;
+    }
+
+    public final class PerkDelegate implements CellDelegate<DtlOfferPerkData> {
+        @Override
+        public void onCellClicked(DtlOfferPerkData perk) {
+            getPresenter().perkClick(perk);
+        }
+    }
+
+    public final class PointsDelegate implements CellDelegate<DtlOfferPointsData> {
+        @Override
+        public void onCellClicked(DtlOfferPointsData points) {
+            getPresenter().pointClicked(points);
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////

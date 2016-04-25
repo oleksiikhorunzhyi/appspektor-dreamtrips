6f6f813ddc05ffd90bb912d78b39e97c706f3b14
dmitriy.m <dmitry.m@techery.io>
2016-04-25 19:21:16 +0300

    Social :: fixed suggestion gallery, added possibility to add new photos

--- app/src/main/java/com/worldventures/dreamtrips/modules/feed/view/cell/delegate/SuggestedPhotosDelegate.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/feed/view/cell/delegate/SuggestedPhotosDelegate.java
@@ -1,5 +1,7 @@
 package com.worldventures.dreamtrips.modules.feed.view.cell.delegate;
 
+import android.database.ContentObserver;
+
 import com.techery.spares.ui.view.cell.CellDelegate;
 import com.worldventures.dreamtrips.modules.common.model.MediaAttachment;
 import com.worldventures.dreamtrips.modules.common.model.PhotoGalleryModel;
@@ -10,4 +12,6 @@ public interface SuggestedPhotosDelegate extends CellDelegate<MediaAttachment> {
     void onCancelClicked();
 
     void onAttachClicked(List<PhotoGalleryModel> pickedItems);
+
+    void onRegisterObserver(ContentObserver contentObserver);
 }

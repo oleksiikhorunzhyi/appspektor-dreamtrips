9aa73a001e2f72fd94fec161781d6ccd53ba88d8
devAnatolii <shadownshadow@yandex.ua>
2016-04-26 14:01:26 +0300

    Messenger :: add util classes for testing

---
+++ app/src/test/java/com/messenger/util/BaseTest.java
@@ -0,0 +1,19 @@
+package com.messenger.util;
+
+import rx.Scheduler;
+import rx.plugins.RxJavaPlugins;
+import rx.plugins.RxJavaSchedulersHook;
+import rx.schedulers.Schedulers;
+
+public class BaseTest {
+
+    static {
+        RxJavaPlugins.getInstance().registerSchedulersHook(new RxJavaSchedulersHook() {
+            @Override
+            public Scheduler getIOScheduler() {
+                return Schedulers.immediate();
+            }
+        });
+    }
+
+}

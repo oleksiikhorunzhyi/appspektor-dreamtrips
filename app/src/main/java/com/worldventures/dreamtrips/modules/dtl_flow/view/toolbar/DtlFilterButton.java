948cae186f651bd10406937a67282a64a2173b39
Den Drobiazko <den.d@techery.io>
2016-04-27 21:12:09 +0300

    DTL :: implement custom view that wraps behaviour

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/view/toolbar/DtlFilterButton.java
@@ -0,0 +1,89 @@
+package com.worldventures.dreamtrips.modules.dtl_flow.view.toolbar;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.worldventures.dreamtrips.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import icepick.Icepick;
+import icepick.State;
+
+public class DtlFilterButton extends FrameLayout {
+
+    private final int enabledBackgroundColor;
+    private final int disabledBackgroundColor;
+    private final int enabledCaptionColor;
+    private final int disabledCaptionColor;
+
+    @InjectView(R.id.dtlfb_rootView)
+    ViewGroup rootView;
+    @InjectView(R.id.dtlfb_caption)
+    AppCompatTextView caption;
+    @InjectView(R.id.dtlfb_separator)
+    View separator;
+
+    @State
+    protected boolean filterEnabled = false;
+
+    public DtlFilterButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        enabledBackgroundColor =
+                getResources().getColor(R.color.dtlfb_enabled_background_color,null);
+        disabledBackgroundColor =
+                getResources().getColor(R.color.dtlfb_disabled_background_color,null);
+        enabledCaptionColor = getResources().getColor(R.color.dtlfb_enabled_caption_color, null);
+        disabledCaptionColor = getResources().getColor(R.color.dtlfb_disabled_caption_color, null);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.inject(this);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        redrawState();
+    }
+
+    protected void redrawState() {
+        if (filterEnabled) {
+            rootView.setBackgroundColor(enabledBackgroundColor);
+            caption.setTextColor(enabledCaptionColor);
+            caption.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    R.drawable.ic_dtl_filter_button_enabled, 0, 0, 0);
+            separator.setVisibility(INVISIBLE);
+        } else {
+            rootView.setBackgroundColor(disabledBackgroundColor);
+            caption.setTextColor(disabledCaptionColor);
+            caption.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    R.drawable.ic_dtl_filter_button_disabled, 0, 0, 0);
+            separator.setVisibility(VISIBLE);
+        }
+    }
+
+    public void setFilterEnabled(final boolean enabled) {
+        filterEnabled = enabled;
+        redrawState();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // State saving
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Override public Parcelable onSaveInstanceState() {
+        return Icepick.saveInstanceState(this, super.onSaveInstanceState());
+    }
+
+    @Override public void onRestoreInstanceState(Parcelable state) {
+        super.onRestoreInstanceState(Icepick.restoreInstanceState(this, state));
+    }
+}

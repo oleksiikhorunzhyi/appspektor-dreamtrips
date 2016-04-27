b1294126580c10c10ae40c1a8af299807435aeba
Den Drobiazko <den.d@techery.io>
2016-04-27 23:31:31 +0300

    DTL :: DT-1645 :: implement fullscreen image screen

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/fullscreen_image/DtlFullscreenImagePath.java
@@ -0,0 +1,29 @@
+package com.worldventures.dreamtrips.modules.dtl_flow.parts.fullscreen_image;
+
+import com.worldventures.dreamtrips.R;
+import com.worldventures.dreamtrips.core.flow.util.Layout;
+import com.worldventures.dreamtrips.modules.dtl_flow.DtlMasterPath;
+
+@Layout(R.layout.screen_dtl_fullscreen_image)
+public class DtlFullscreenImagePath extends DtlMasterPath {
+
+    private final String url;
+
+    public DtlFullscreenImagePath(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public boolean isFullScreen() {
+        return true;
+    }
+
+    @Override
+    public PathAttrs getAttrs() {
+        return WITHOUT_DRAWER;
+    }
+}
---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/fullscreen_image/DtlFullscreenImagePresenter.java
@@ -0,0 +1,8 @@
+package com.worldventures.dreamtrips.modules.dtl_flow.parts.fullscreen_image;
+
+import com.worldventures.dreamtrips.modules.dtl_flow.DtlPresenter;
+import com.worldventures.dreamtrips.modules.dtl_flow.ViewState;
+
+public interface DtlFullscreenImagePresenter extends DtlPresenter<DtlFullscreenImageScreen,
+        ViewState.EMPTY> {
+}

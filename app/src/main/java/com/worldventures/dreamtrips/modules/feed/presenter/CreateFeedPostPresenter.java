023982d6ec0495a163bae6ca939846d94805af25
kput4uk@gmail.com <kput4uk@gmail.com>
2016-04-28 18:32:31 +0300

    Social :: Fix photo rotation on samsung in photo suggestions

--- app/src/main/java/com/worldventures/dreamtrips/modules/feed/presenter/CreateFeedPostPresenter.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/feed/presenter/CreateFeedPostPresenter.java
@@ -2,9 +2,11 @@ package com.worldventures.dreamtrips.modules.feed.presenter;
 
 public class CreateFeedPostPresenter extends CreateEntityPresenter<CreateFeedPostPresenter.View> {
 
+    public static final int REQUEST_ID = CreateFeedPostPresenter.class.getSimpleName().hashCode();
+
     @Override
     public int getMediaRequestId() {
-        return CreateFeedPostPresenter.class.getSimpleName().hashCode();
+        return REQUEST_ID;
     }
 
     public interface View extends CreateEntityPresenter.View {

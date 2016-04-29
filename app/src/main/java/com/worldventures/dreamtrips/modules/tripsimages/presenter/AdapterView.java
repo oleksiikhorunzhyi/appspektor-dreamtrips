a16cae2790a6e4ce6957d95b51e78bb84e2d156e
kput4uk@gmail.com <kput4uk@gmail.com>
2016-04-29 11:18:38 +0300

    Social :: Notify list after upload

--- app/src/main/java/com/worldventures/dreamtrips/modules/tripsimages/presenter/AdapterView.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/tripsimages/presenter/AdapterView.java
@@ -1,9 +1,14 @@
 package com.worldventures.dreamtrips.modules.tripsimages.presenter;
 
+import java.util.List;
+
 public interface AdapterView<T> {
+
     void add(T item);
 
     void add(int position, T item);
+
+    void addAll(int position, List<? extends T> items);
 
     void clear();
 

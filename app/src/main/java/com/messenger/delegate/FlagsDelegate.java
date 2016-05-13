9f19a992daabe7ae1d5431907316f0958fa242db
edward <palevomr@gmail.com>
2016-05-13 16:20:45 +0900

    Remove needless RetryLoginComposer from messenger related delegates

--- app/src/main/java/com/messenger/delegate/FlagsDelegate.java
+++ app/src/main/java/com/messenger/delegate/FlagsDelegate.java
@@ -1,10 +1,7 @@
 package com.messenger.delegate;
 
-import com.messenger.api.temp.RetryLoginComposer;
-import com.techery.spares.session.SessionHolder;
 import com.messenger.api.GetFlagsAction;
 import com.worldventures.dreamtrips.core.rx.composer.NonNullFilter;
-import com.worldventures.dreamtrips.core.session.UserSession;
 
 import io.techery.janet.ActionPipe;
 import io.techery.janet.Janet;
@@ -14,21 +11,16 @@ import timber.log.Timber;
 
 public class FlagsDelegate {
 
-    private final SessionHolder<UserSession> sessionHolder;
-    private Janet janet;
     private ActionPipe<GetFlagsAction> flagsPipe;
     private GetFlagsAction getFlagsAction;
 
-    public FlagsDelegate(SessionHolder<UserSession> sessionHolder, Janet janet) {
-        this.sessionHolder = sessionHolder;
-        this.janet = janet;
+    public FlagsDelegate(Janet janet) {
         flagsPipe = janet.createPipe(GetFlagsAction.class, Schedulers.io());
     }
 
     public Observable<GetFlagsAction> getFlags() {
         Observable<GetFlagsAction> observable =
                 flagsPipe.createObservableSuccess(new GetFlagsAction())
-                .compose(new RetryLoginComposer<>(sessionHolder, janet))
                 .startWith(getFlagsAction)
                 .compose(new NonNullFilter<>())
                 .take(1)
--- app/src/main/java/com/messenger/di/FlaggingModule.java
+++ app/src/main/java/com/messenger/di/FlaggingModule.java
@@ -1,8 +1,6 @@
 package com.messenger.di;
 
-import com.techery.spares.session.SessionHolder;
 import com.messenger.delegate.FlagsDelegate;
-import com.worldventures.dreamtrips.core.session.UserSession;
 
 import javax.inject.Singleton;
 
@@ -18,7 +16,7 @@ public class FlaggingModule {
 
     @Provides
     @Singleton
-    FlagsDelegate provideFlagsProvider(SessionHolder<UserSession> sessionHolder, Janet janet) {
-        return new FlagsDelegate(sessionHolder, janet);
+    FlagsDelegate provideFlagsProvider(Janet janet) {
+        return new FlagsDelegate(janet);
     }
 }

d7df0ff4b4352c49f894b25ad6b93eb08148c58a
man4ik <mensutove@gmail.com>
2016-05-16 18:12:01 +0300

    Add real trip details request for map clastering

--- app/src/main/java/com/worldventures/dreamtrips/modules/trips/api/GetDetailedTripsAction.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/trips/api/GetDetailedTripsAction.java
@@ -1,5 +1,6 @@
 package com.worldventures.dreamtrips.modules.trips.api;
 
+import com.worldventures.dreamtrips.core.api.action.BaseHttpAction;
 import com.worldventures.dreamtrips.modules.trips.model.TripModel;
 
 import java.util.List;
@@ -9,14 +10,17 @@ import io.techery.janet.http.annotations.Query;
 import io.techery.janet.http.annotations.Response;
 
 @HttpAction(value = "/api/trips/details", type = HttpAction.Type.SIMPLE, method = HttpAction.Method.GET)
-public class GetDetailedTripsAction  {
+public class GetDetailedTripsAction extends BaseHttpAction {
 
-    @Query("ids") List<String> ids;
+    @Query("ids")
+    String ids;
 
-    @Response List<TripModel> tripList;
+    @Response
+    List<TripModel> tripList;
 
     public GetDetailedTripsAction(List<String> tripUids) {
-        ids = tripUids;
+        ids = android.text.TextUtils.join(",", tripUids);
+
     }
 
     public List<TripModel> getTripList() {

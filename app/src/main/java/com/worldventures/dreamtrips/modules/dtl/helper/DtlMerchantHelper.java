98fb4a29b215560d0a023a85d754840474c7a228
Alex Khorunzhyi <alex.kh@techery.io>
2016-06-15 18:09:03 +0300

    DTL :: fix format expiration date

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/helper/DtlMerchantHelper.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/helper/DtlMerchantHelper.java
@@ -78,7 +78,7 @@ public class DtlMerchantHelper {
     public static Spannable getOfferExpiringCaption(Resources resources, DtlOffer offerData) {
         String format = resources.getString(R.string.offer_expiration_format);
         DateTime expiringDate = new DateTime(offerData.getEndDate().getTime());
-        String caption = expiringDate.toString(DateTimeFormat.forPattern("d MMM"));
+        String caption = expiringDate.toString(DateTimeFormat.forPattern("MMMM d"));
         String captionFormatted = String.format(format, caption);
         Spannable spanned = new SpannableString(captionFormatted);
         spanned.setSpan(new StyleSpan(Typeface.BOLD),

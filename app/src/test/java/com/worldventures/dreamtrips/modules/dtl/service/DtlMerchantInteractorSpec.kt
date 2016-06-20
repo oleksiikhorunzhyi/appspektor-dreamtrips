58218e0cc98f7ffe57483ebebb26be3e9beb804d
dirong <alex.vl.kozlov@gmail.com>
2016-06-20 15:30:41 +0300

    DTL :: Core :: Testing spec as approach to ease describe test cases

---
+++ app/src/test/java/com/worldventures/dreamtrips/modules/dtl/service/DtlMerchantInteractorSpec.kt
@@ -0,0 +1,65 @@
+package com.worldventures.dreamtrips.modules.dtl.service
+
+import com.messenger.util.AssertUtil.assertActionSuccess
+import com.nhaarman.mockito_kotlin.anyCollection
+import com.nhaarman.mockito_kotlin.verify
+import com.worldventures.dreamtrips.modules.dtl.service.action.DtlFilterDataAction
+import com.worldventures.dreamtrips.modules.dtl.service.action.DtlLocationCommand
+import com.worldventures.dreamtrips.modules.dtl.service.action.DtlMerchantByIdAction
+import com.worldventures.dreamtrips.modules.dtl.service.action.DtlUpdateAmenitiesAction
+import io.techery.janet.ActionState
+import rx.observers.TestSubscriber
+
+class DtlMerchantInteractorSpec : DtlBaseMerchantSpec({
+
+    val updateAmenitiesSubscriber = TestSubscriber<ActionState<DtlUpdateAmenitiesAction>>()
+    val filterDataSubscriber = TestSubscriber<ActionState<DtlFilterDataAction>>()
+    val locationSubscriber = TestSubscriber<ActionState<DtlLocationCommand>>()
+
+    given("Setup") {
+        on("init basic variables") {
+            initVars()
+        }
+        on("subscribe DtlUpdateAmenitiesAction") {
+            janet.createPipe(DtlUpdateAmenitiesAction::class.java).observe()
+                    .subscribe(updateAmenitiesSubscriber)
+        }
+        on("subscribe DtlFilterDataAction") {
+            janet.createPipe(DtlFilterDataAction::class.java).observe()
+                    .subscribe(filterDataSubscriber)
+        }
+        on("subscribe DtlLocationCommand") {
+            janet.createPipe(DtlLocationCommand::class.java).observe().subscribe(locationSubscriber)
+        }
+    }
+
+    describe("Sending DtlMerchantsAction") {
+        it("should send request to load merchants") {
+            checkMerchantActionLoad()
+        }
+        it("should get merchants from cache") {
+            checkMerchantActionRestore()
+        }
+        it("checks updating amenities which happened after loading merchants") {
+            updateAmenitiesSubscriber.unsubscribe()
+            assertActionSuccess(updateAmenitiesSubscriber) { action -> action.result != null }
+            verify(db).saveAmenities(anyCollection())
+        }
+        it("checks changing filter after loading merchants") {
+            filterDataSubscriber.unsubscribe()
+            assertActionSuccess(filterDataSubscriber) { action -> action.result != null }
+        }
+        it("checks changing location after loading merchants") {
+            locationSubscriber.unsubscribe()
+            assertActionSuccess(locationSubscriber) { action -> action.result != null }
+        }
+        it("should get merchant by mocked id") {
+            val subscriber = TestSubscriber<ActionState<DtlMerchantByIdAction>>()
+            merchantInteractor.merchantByIdPipe()
+                    .createObservable(DtlMerchantByIdAction(DtlBaseMerchantSpec.MERCHANT_ID))
+                    .subscribe(subscriber)
+            assertActionSuccess(subscriber) { action -> action.result != null }
+        }
+    }
+})
+

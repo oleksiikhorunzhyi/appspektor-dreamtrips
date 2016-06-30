2c2c3e8f79a718452bda651e6b13a2d34f33df0b
Roman Minenok <roman.minenok@gmail.com>
2016-06-30 15:05:00 +0300

    Rename default message type

--- app/src/main/java/com/messenger/messengerservers/constant/MessageType.java
+++ app/src/main/java/com/messenger/messengerservers/constant/MessageType.java
@@ -3,12 +3,12 @@ package com.messenger.messengerservers.constant;
 import android.support.annotation.StringDef;
 
 public class MessageType {
-    public static final String USER = "user";
+    public static final String MESSAGE = "message";
     public static final String SYSTEM_JOIN = "join";
     public static final String SYSTEM_LEAVE = "leave";
     public static final String SYSTEM_KICK = "kick";
 
-    @StringDef({USER, SYSTEM_JOIN, SYSTEM_LEAVE, SYSTEM_KICK})
+    @StringDef({MESSAGE, SYSTEM_JOIN, SYSTEM_LEAVE, SYSTEM_KICK})
     public @interface Type {
     }
 }

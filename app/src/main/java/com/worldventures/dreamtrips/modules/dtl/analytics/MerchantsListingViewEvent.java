a9dc5dbe23704832c4c2a6d93ccbcc9759576f36
Den Drobiazko <den.d@techery.io>
2016-07-12 18:09:55 +0300

    DTL :: fix analytics event name lowercase

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/analytics/MerchantsListingViewEvent.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/analytics/MerchantsListingViewEvent.java
@@ -3,6 +3,6 @@ package com.worldventures.dreamtrips.modules.dtl.analytics;
 import com.worldventures.dreamtrips.core.utils.tracksystem.AdobeTracker;
 import com.worldventures.dreamtrips.core.utils.tracksystem.AnalyticsEvent;
 
-@AnalyticsEvent(action = "Local:Restaurant-Listings", trackers = AdobeTracker.TRACKER_KEY)
+@AnalyticsEvent(action = "local:Restaurant-Listings", trackers = AdobeTracker.TRACKER_KEY)
 public class MerchantsListingViewEvent extends DtlAnalyticsAction {
 }

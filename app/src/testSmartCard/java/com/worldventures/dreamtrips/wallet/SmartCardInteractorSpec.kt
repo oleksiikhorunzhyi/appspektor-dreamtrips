95d40de6df70d5bbf6128abbce06f34226c5b9d4
dmitriy.m <dmitry.m@techery.io>
2016-07-25 12:49:37 +0300

    SmartCard :: added smart-card-specific test folder with stubbed test class

---
+++ app/src/testSmartCard/java/com/worldventures/dreamtrips/wallet/SmartCardInteractorSpec.kt
@@ -0,0 +1,9 @@
+package com.worldventures.dreamtrips.wallet
+
+import com.worldventures.dreamtrips.BaseSpec
+
+class SmartCardInteractorSpec : BaseSpec({
+    //spec tests
+}) {
+    //class body
+}

7f262fc89d6cc904854f59a06503ce4aba7e65af
Dmitry Sovgyr <dimdron@gmail.com>
2016-07-28 10:39:19 +0300

    Wallet :: remove base class for toolbar

--- app/src/main/java/com/worldventures/dreamtrips/wallet/ui/wizard/card_alias/WizardCardNameScreen.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/wizard/card_alias/WizardCardNameScreen.java
@@ -2,18 +2,22 @@ package com.worldventures.dreamtrips.wallet.ui.wizard.card_alias;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import com.worldventures.dreamtrips.R;
-import com.worldventures.dreamtrips.wallet.ui.common.base.BaseToolbarScreenLayout;
+import com.worldventures.dreamtrips.wallet.ui.common.base.WalletFrameLayout;
 
+import butterknife.InjectView;
 import butterknife.OnClick;
 import butterknife.OnEditorAction;
 
-public class WizardCardNameScreen extends BaseToolbarScreenLayout<WizardCardNamePresenter.Screen, WizardCardNamePresenter, WizardCardAliasPath>
+public class WizardCardNameScreen extends WalletFrameLayout<WizardCardNamePresenter.Screen, WizardCardNamePresenter, WizardCardAliasPath>
         implements WizardCardNamePresenter.Screen {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
     public WizardCardNameScreen(Context context) {
         super(context);
@@ -30,8 +34,12 @@ public class WizardCardNameScreen extends BaseToolbarScreenLayout<WizardCardName
     }
 
     @Override
-    protected void onNavigateButtonClick(View view) {
-        super.onNavigateButtonClick(view);
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        toolbar.setNavigationOnClickListener(v -> navigateButtonClick());
+    }
+
+    protected void navigateButtonClick() {
         presenter.goToBack();
     }
 
--- app/src/main/java/com/worldventures/dreamtrips/wallet/ui/wizard/profile/WizardEditProfileScreen.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/wizard/profile/WizardEditProfileScreen.java
@@ -2,17 +2,20 @@ package com.worldventures.dreamtrips.wallet.ui.wizard.profile;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import com.worldventures.dreamtrips.R;
-import com.worldventures.dreamtrips.wallet.ui.common.base.BaseToolbarScreenLayout;
+import com.worldventures.dreamtrips.wallet.ui.common.base.WalletFrameLayout;
 
+import butterknife.InjectView;
 import butterknife.OnClick;
 import butterknife.OnEditorAction;
 
-public class WizardEditProfileScreen extends BaseToolbarScreenLayout<WizardEditProfilePresenter.Screen, WizardEditProfilePresenter, WizardEditProfilePath> implements WizardEditProfilePresenter.Screen {
+public class WizardEditProfileScreen extends WalletFrameLayout<WizardEditProfilePresenter.Screen, WizardEditProfilePresenter, WizardEditProfilePath> implements WizardEditProfilePresenter.Screen {
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
     public WizardEditProfileScreen(Context context) {
         super(context);
@@ -29,7 +32,12 @@ public class WizardEditProfileScreen extends BaseToolbarScreenLayout<WizardEditP
     }
 
     @Override
-    protected void onNavigateButtonClick(View view) {
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        toolbar.setNavigationOnClickListener(v -> navigateButtonClick());
+    }
+
+    protected void navigateButtonClick() {
         presenter.goToBack();
     }
 

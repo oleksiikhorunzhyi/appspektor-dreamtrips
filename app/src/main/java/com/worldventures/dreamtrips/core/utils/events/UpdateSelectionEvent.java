56ea6c9461e0ad83ee7262459159dbd85412672c
edward <palevomr@gmail.com>
2016-07-28 21:13:15 +0900

    Remove unused eventBus shit

--- app/src/main/java/com/worldventures/dreamtrips/core/utils/events/UpdateSelectionEvent.java
+++
@@ -1,7 +0,0 @@
-package com.worldventures.dreamtrips.core.utils.events;
-
-/**
- *  1 on 06.02.15.
- */
-public class UpdateSelectionEvent {
-}
--- app/src/main/java/com/worldventures/dreamtrips/core/utils/events/WebViewReloadEvent.java
+++
@@ -1,8 +0,0 @@
-package com.worldventures.dreamtrips.core.utils.events;
-
-
-public class WebViewReloadEvent {
-    public WebViewReloadEvent() {
-        //nothing to do here
-    }
-}

9509c20e29773b46d334e96b1a4c3c22cfcc7b57
Den Drobiazko <den.d@techery.io>
2016-08-04 10:08:42 +0300

    DTL :: explicitly use `toLowerCase`
    Explicitly use `toLowerCase` in enums instead of hidden and overridden - latter is non-obvious and non-reliable

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/MerchantType.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/MerchantType.java
@@ -1,12 +1,5 @@
 package com.worldventures.dreamtrips.modules.dtl.model.merchant;
 
-import java.util.Locale;
-
 public enum MerchantType {
-    RESTAURANT, BAR, UNKNOWN;
-
-    @Override
-    public String toString() {
-        return super.toString().toLowerCase(Locale.US);
-    }
+    RESTAURANT, BAR, UNKNOWN
 }
--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/PartnerStatus.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/PartnerStatus.java
@@ -1,12 +1,5 @@
 package com.worldventures.dreamtrips.modules.dtl.model.merchant;
 
-import java.util.Locale;
-
 public enum PartnerStatus {
-    PARTICIPANT, PROSPECT, EXCLUDED, PENDING, UNKNOWN;
-
-    @Override
-    public String toString() {
-        return super.toString().toLowerCase(Locale.US);
-    }
+    PARTICIPANT, PROSPECT, EXCLUDED, PENDING, UNKNOWN
 }

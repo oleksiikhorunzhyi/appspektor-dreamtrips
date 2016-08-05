229613c07c0cf1e6440852b82dc2c936cdb318c2
Dmitry <dimdron@gmail.com>
2016-08-05 18:32:56 +0300

    SmartCard :: Fix barcode view to be consistent with UI (#2065)

---
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/widget/WalletBarCodeFinder.java
@@ -0,0 +1,17 @@
+package com.worldventures.dreamtrips.wallet.ui.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import com.worldventures.dreamtrips.R;
+
+import me.dm7.barcodescanner.core.ViewFinderView;
+
+public class WalletBarCodeFinder extends ViewFinderView {
+
+    public WalletBarCodeFinder(Context context) {
+        super(context);
+        mBorderPaint.setColor(Color.WHITE);
+        mBorderLineLength = context.getResources().getDimensionPixelOffset(R.dimen.wallet_wizard_bar_code_border_size);
+    }
+}

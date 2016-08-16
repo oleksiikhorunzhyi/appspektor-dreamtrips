86a9e1077c55776703307995ebbd9cc2e4ddd85a
Dmitry Sovgyr <dimdron@gmail.com>
2016-08-16 18:28:21 +0300

    SmartCard :: fix connect command

--- app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/ConnectSmartCardCommand.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/ConnectSmartCardCommand.java
@@ -2,6 +2,7 @@ package com.worldventures.dreamtrips.wallet.service.command;
 
 import com.worldventures.dreamtrips.core.janet.JanetModule;
 import com.worldventures.dreamtrips.core.janet.dagger.InjectableAction;
+import com.worldventures.dreamtrips.core.repository.SnappyRepository;
 import com.worldventures.dreamtrips.wallet.domain.entity.SmartCard;
 
 import javax.inject.Inject;
@@ -17,6 +18,7 @@ import rx.Observable;
 public class ConnectSmartCardCommand extends Command<SmartCard> implements InjectableAction, SmartCardModifier {
 
     @Inject @Named(JanetModule.JANET_WALLET) Janet janet;
+    @Inject SnappyRepository snappyRepository;
 
     private final SmartCard activeSmartCard;
 
@@ -29,6 +31,7 @@ public class ConnectSmartCardCommand extends Command<SmartCard> implements Injec
         janet.createPipe(ConnectAction.class)
                 .createObservableResult(new ConnectAction(activeSmartCard.deviceName(), activeSmartCard.deviceAddress()))
                 .flatMap(action -> fetchTechnicalProperties())
+                .doOnNext(snappyRepository::saveSmartCard)
                 .subscribe(callback::onSuccess, callback::onFail);
     }
 

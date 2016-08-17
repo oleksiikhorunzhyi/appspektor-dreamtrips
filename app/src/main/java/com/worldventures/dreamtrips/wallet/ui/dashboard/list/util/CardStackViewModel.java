27b3e89559dfc81c10d84114a4950b310d2e1777
Aleksey Malevaniy <a.malevaniy@gmail.com>
2016-08-17 18:15:53 +0300

    Fix cards loaded counter (skip default card)

--- app/src/main/java/com/worldventures/dreamtrips/wallet/ui/dashboard/list/util/CardStackViewModel.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/dashboard/list/util/CardStackViewModel.java
@@ -4,17 +4,23 @@ import android.support.annotation.Nullable;
 
 import com.techery.spares.adapter.HeaderItem;
 import com.worldventures.dreamtrips.wallet.domain.entity.card.BankCard;
+import com.worldventures.dreamtrips.wallet.service.command.CardStacksCommand.CardStackModel.StackType;
 
 import java.util.List;
 
 public class CardStackViewModel implements HeaderItem {
+   private StackType type;
+   private List<BankCard> cardList;
    private String title;
 
-   private List<BankCard> cardList;
-
-   public CardStackViewModel(String title, List<BankCard> cardList) {
-      this.title = title;
+   public CardStackViewModel(StackType type, List<BankCard> cardList, String title) {
+      this.type = type;
       this.cardList = cardList;
+      this.title = title;
+   }
+
+   public StackType getType() {
+      return type;
    }
 
    public List<BankCard> getCardList() {

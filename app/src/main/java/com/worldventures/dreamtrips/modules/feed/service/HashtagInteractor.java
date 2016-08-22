ac08809027c2abf996c1186480d94c9c5c620724
Roman Minenok <roman.minenok@gmail.com>
2016-08-22 13:54:34 +0300

    Fix new post text persistance on rotation

--- app/src/main/java/com/worldventures/dreamtrips/modules/feed/service/HashtagInteractor.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/feed/service/HashtagInteractor.java
@@ -2,7 +2,6 @@ package com.worldventures.dreamtrips.modules.feed.service;
 
 import com.worldventures.dreamtrips.modules.feed.service.command.FeedByHashtagCommand;
 import com.worldventures.dreamtrips.modules.feed.service.command.HashtagSuggestionCommand;
-import com.worldventures.dreamtrips.modules.feed.service.command.PostDescriptionCreatedCommand;
 
 import javax.inject.Inject;
 
@@ -12,24 +11,18 @@ import io.techery.janet.Janet;
 public class HashtagInteractor {
 
    private final ActionPipe<HashtagSuggestionCommand> suggestionPipe;
-   private final ActionPipe<PostDescriptionCreatedCommand> descPickedPipe;
    private final ActionPipe<FeedByHashtagCommand.Refresh> refreshFeedsByHashtagsPipe;
    private final ActionPipe<FeedByHashtagCommand.LoadNext> loadNextFeedsByHashtagsPipe;
 
    @Inject
    public HashtagInteractor(Janet janet) {
       suggestionPipe = janet.createPipe(HashtagSuggestionCommand.class);
-      descPickedPipe = janet.createPipe(PostDescriptionCreatedCommand.class);
       refreshFeedsByHashtagsPipe = janet.createPipe(FeedByHashtagCommand.Refresh.class);
       loadNextFeedsByHashtagsPipe = janet.createPipe(FeedByHashtagCommand.LoadNext.class);
    }
 
    public ActionPipe<HashtagSuggestionCommand> getSuggestionPipe() {
       return suggestionPipe;
-   }
-
-   public ActionPipe<PostDescriptionCreatedCommand> getDescPickedPipe() {
-      return descPickedPipe;
    }
 
    public ActionPipe<FeedByHashtagCommand.Refresh> getRefreshFeedsByHashtagsPipe() {

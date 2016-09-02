fe052f11e01ec02d878a70c7d85aa4597bf7dbd6
Alex Khorunzhyi <alex.kh@techery.io>
2016-09-02 17:50:19 +0300

    DTL :: offer models immutables

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/operational_hour/OperationHours.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/model/merchant/operational_hour/OperationHours.java
@@ -1,62 +1,14 @@
 package com.worldventures.dreamtrips.modules.dtl.model.merchant.operational_hour;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.esotericsoftware.kryo.DefaultSerializer;
 import com.esotericsoftware.kryo.serializers.CompatibleFieldSerializer;
 
+import org.immutables.value.Value;
+
 @DefaultSerializer(CompatibleFieldSerializer.class)
-public class OperationHours implements Parcelable {
+@Value.Immutable
+public interface OperationHours {
 
-   private String from;
-   private String to;
-
-   public OperationHours() {
-   }
-
-   public OperationHours(com.worldventures.dreamtrips.api.dtl.merchants.model.OperationHours operationHours) {
-      from = operationHours.fromTime();
-      to = operationHours.toTime();
-   }
-
-   public String getFrom() {
-      return from;
-   }
-
-   public String getTo() {
-      return to;
-   }
-
-   ///////////////////////////////////////////////////////////////////////////
-   // Parcelable
-   ///////////////////////////////////////////////////////////////////////////
-
-   protected OperationHours(Parcel in) {
-      from = in.readString();
-      to = in.readString();
-   }
-
-   @Override
-   public void writeToParcel(Parcel dest, int flags) {
-      dest.writeString(from);
-      dest.writeString(to);
-   }
-
-   @Override
-   public int describeContents() {
-      return 0;
-   }
-
-   public static final Creator<OperationHours> CREATOR = new Creator<OperationHours>() {
-      @Override
-      public OperationHours createFromParcel(Parcel in) {
-         return new OperationHours(in);
-      }
-
-      @Override
-      public OperationHours[] newArray(int size) {
-         return new OperationHours[size];
-      }
-   };
+   String from();
+   String to();
 }

a0ccee026d61921467445ef9e8596acc81a3ad2c
Roman Minenok <roman.m@techery.io>
2016-09-15 16:14:26 +0700

    Split permissions for OTA and booking trips

--- app/src/main/java/com/worldventures/dreamtrips/core/session/acl/Feature.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/session/acl/Feature.java
@@ -14,7 +14,7 @@ public class Feature {
    }
 
    @Retention(SOURCE)
-   @StringDef({TRIPS, REP_TOOLS, SOCIAL, DTL, REP_SUGGEST_MERCHANT, BOOK_TRAVEL, MEMBERSHIP})
+   @StringDef({TRIPS, REP_TOOLS, SOCIAL, DTL, REP_SUGGEST_MERCHANT, BOOK_TRAVEL, BOOK_TRIP, MEMBERSHIP})
    public @interface FeatureName {}
 
    public static final String TRIPS = "trips";
@@ -23,6 +23,7 @@ public class Feature {
    public static final String DTL = "discover";
    public static final String REP_SUGGEST_MERCHANT = "repSuggestMerchant";
    public static final String BOOK_TRAVEL = "bookTravel";
+   public static final String BOOK_TRIP = "bookTrip";
    public static final String MEMBERSHIP = "membership";
 
 }

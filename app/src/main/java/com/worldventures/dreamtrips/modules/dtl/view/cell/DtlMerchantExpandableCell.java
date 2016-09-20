32c120fdb9ec1a3645a76ebf3a2dc423a9c456c9
Alex Khorunzhyi <alex.kh@techery.io>
2016-09-20 14:20:49 +0300

    DTL :: change expandable cell for thin merchant

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/cell/DtlMerchantExpandableCell.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/cell/DtlMerchantExpandableCell.java
@@ -20,11 +20,8 @@ import com.worldventures.dreamtrips.core.utils.LocaleHelper;
 import com.worldventures.dreamtrips.core.utils.ViewUtils;
 import com.worldventures.dreamtrips.core.utils.tracksystem.AnalyticsInteractor;
 import com.worldventures.dreamtrips.modules.common.view.custom.ImageryDraweeView;
-import com.worldventures.dreamtrips.modules.dtl.analytics.DtlAnalyticsCommand;
-import com.worldventures.dreamtrips.modules.dtl.analytics.MerchantsListingExpandEvent;
 import com.worldventures.dreamtrips.modules.dtl.helper.MerchantHelper;
-import com.worldventures.dreamtrips.modules.dtl.model.DistanceType;
-import com.worldventures.dreamtrips.modules.dtl.model.merchant.DtlMerchant;
+import com.worldventures.dreamtrips.modules.dtl.model.merchant.ImmutableThinMerchant;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.MerchantMedia;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.offer.Offer;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.operational_hour.OperationDay;
@@ -40,7 +37,7 @@ import io.techery.properratingbar.ProperRatingBar;
 import rx.Observable;
 
 @Layout(R.layout.adapter_item_dtl_merchant_expandable)
-public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant, DtlMerchantCellDelegate> {
+public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThinMerchant, DtlMerchantCellDelegate> {
 
    @InjectView(R.id.merchantCoverImage) ImageryDraweeView merchantCoverImage;
    @InjectView(R.id.merchantPricing) ProperRatingBar pricing;
@@ -67,10 +64,10 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
 
    @Override
    protected void syncUIStateWithModel() {
-      merchantName.setText(getModelObject().getDisplayName());
-      pricing.setRating(getModelObject().getBudget());
+      merchantName.setText(getModelObject().displayName());
+      pricing.setRating(getModelObject().budget());
       //
-      setImage(getModelObject().getImages());
+      setImage(getModelObject().images());
       setDistance();
       setCategories();
       setOperationalStatus();
@@ -91,8 +88,9 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
 
    private void setDistance() {
       Resources res = merchantDistance.getResources();
-      ViewUtils.setTextOrHideView(merchantDistance, res.getString(R.string.distance_caption_format, getModelObject().getDistance(), res
-            .getString(getModelObject().getDistanceType() == DistanceType.MILES ? R.string.mi : R.string.km)));
+      // TODO :: set distance
+//      ViewUtils.setTextOrHideView(merchantDistance, res.getString(R.string.distance_caption_format, getModelObject().getDistance(), res
+//            .getString(getModelObject().getDistanceType() == DistanceType.MILES ? R.string.mi : R.string.km)));
    }
 
    private void setCategories() {
@@ -101,8 +99,7 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
    }
 
    private void setOperationalStatus() {
-      if (getModelObject().hasPoints() && getModelObject().getOperationDays() != null && !getModelObject().getOperationDays()
-            .isEmpty()) {
+      if (getModelObject().hasPoints() && getModelObject().hasOperationDays()) {
          ViewUtils.setViewVisibility(merchantOperationalStatus, View.VISIBLE);
          Observable.fromCallable(() -> MerchantHelper.getOperationalTime(itemView.getContext(), getModelObject(), false))
                .compose(RxLifecycle.bindView(itemView))
@@ -111,17 +108,17 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
    }
 
    private void setOffersSection() {
-      if (getModelObject().hasNoOffers()) ViewUtils.setViewVisibility(offersContainer, View.GONE);
+      if (!getModelObject().hasOffers()) ViewUtils.setViewVisibility(offersContainer, View.GONE);
       else {
          ViewUtils.setViewVisibility(offersContainer, View.VISIBLE);
-         int perksNumber = Queryable.from(getModelObject().getOffers()).count(offer -> offer.type() == OfferType.PERK);
-         setOfferBadges(perksNumber, getModelObject().getOffers().size() - perksNumber);
+         int perksNumber = getModelObject().offersCount(OfferType.PERK);
+         setOfferBadges(perksNumber, getModelObject().offers().size() - perksNumber);
       }
    }
 
    private void setOfferBadges(int perks, int points) {
-      int perkVisibility = getModelObject().isExpanded() ? View.GONE : perks > 0 ? View.VISIBLE : View.GONE;
-      int pointVisibility = getModelObject().isExpanded() ? View.GONE : points > 0 ? View.VISIBLE : View.GONE;
+      int perkVisibility = perks > 0 ? View.VISIBLE : View.GONE;
+      int pointVisibility = points > 0 ? View.VISIBLE : View.GONE;
 
       ViewUtils.setViewVisibility(this.perks, perkVisibility);
       ViewUtils.setViewVisibility(this.points, pointVisibility);
@@ -131,10 +128,10 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
    }
 
    private void setToggleView() {
-      int toggleDrawable = getModelObject().isExpanded() ? R.drawable.ic_arrow_up_grey : R.drawable.ic_arrow_down_grey;
-      perkToggleImage.setBackgroundResource(toggleDrawable);
-      //
-      ViewUtils.setViewVisibility(perkToggleText, getModelObject().isExpanded() ? View.VISIBLE : View.GONE);
+//      int toggleDrawable = getModelObject().isExpanded() ? R.drawable.ic_arrow_up_grey : R.drawable.ic_arrow_down_grey;
+//      perkToggleImage.setBackgroundResource(toggleDrawable);
+//      //
+//      ViewUtils.setViewVisibility(perkToggleText, getModelObject().isExpanded() ? View.VISIBLE : View.GONE);
    }
 
    /**
@@ -142,28 +139,28 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<DtlMerchant,
     * TODO :: 5/20/16
     */
    private void setExpandedArea() {
-      if (getModelObject().isExpanded()) {
-         expandedContainer.setVisibility(View.VISIBLE);
-         expandedContainer.removeAllViews();
-         Queryable.from(getModelObject().getOffers()).forEachR(offer -> {
-            if (offer.type() == OfferType.PERK) bindPerkCell(offer, expandedContainer);
-            else bindPointsCell(offer, expandedContainer);
-         });
-      } else {
-         expandedContainer.setVisibility(View.GONE);
-      }
+//      if (getModelObject().isExpanded()) {
+//         expandedContainer.setVisibility(View.VISIBLE);
+//         expandedContainer.removeAllViews();
+//         Queryable.from(getModelObject().getOffers()).forEachR(offer -> {
+//            if (offer.type() == OfferType.PERK) bindPerkCell(offer, expandedContainer);
+//            else bindPointsCell(offer, expandedContainer);
+//         });
+//      } else {
+//         expandedContainer.setVisibility(View.GONE);
+//      }
    }
 
    private View.OnClickListener offerClickedListener = v -> cellDelegate.onOfferClick(getModelObject(), (Offer) v.getTag());
 
    @OnClick(R.id.offers_container)
    void togglExpandClicked() {
-      getModelObject().toggleExpanded();
-      if (getModelObject().isExpanded()) {
-         analyticsInteractor.dtlAnalyticsCommandPipe()
-               .send(DtlAnalyticsCommand.create(new MerchantsListingExpandEvent(getModelObject())));
-      }
-      cellDelegate.onExpandedToggle(getAdapterPosition());
+//      getModelObject().toggleExpanded();
+//      if (getModelObject().isExpanded()) {
+//         analyticsInteractor.dtlAnalyticsCommandPipe()
+//               .send(DtlAnalyticsCommand.create(new MerchantsListingExpandEvent(getModelObject())));
+//      }
+//      cellDelegate.onExpandedToggle(getAdapterPosition());
    }
 
    @OnClick(R.id.merchantCellBodyLayout)

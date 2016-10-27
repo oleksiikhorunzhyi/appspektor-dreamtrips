fc94c3d94bc6f2e9fb162b4c0901801c329d1fda
Roman Minenok <roman.m@techery.io>
2016-10-27 19:55:29 +0700

    Fix mappable base api action command

--- app/src/main/java/com/worldventures/dreamtrips/core/api/action/MappableBaseApiActionCommand.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/api/action/MappableBaseApiActionCommand.java
@@ -7,11 +7,15 @@ import io.techery.mappery.MapperyContext;
 public abstract class MappableBaseApiActionCommand<Action extends com.worldventures.dreamtrips.api.api_common.BaseHttpAction, T, M>
       extends BaseApiActionCommand<Action, T, Object> {
 
-   @Inject MapperyContext mapperyContext;
+   @Inject protected MapperyContext mapperyContext;
 
    @Override
    protected T mapCommandResult(Object httpCommandResult) {
-      return (T) mapperyContext.convert((Iterable<?>) httpCommandResult, getMappingTargetClass());
+      if (httpCommandResult instanceof Iterable) {
+         return (T) mapperyContext.convert((Iterable<?>) httpCommandResult, getMappingTargetClass());
+      } else {
+         return (T) mapperyContext.convert(httpCommandResult, getMappingTargetClass());
+      }
    }
 
    protected abstract Class<M> getMappingTargetClass();

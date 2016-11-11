07fd00784c857b9a193194045507ba15b0564e08
Den Drobiazko <den.d@techery.io>
2016-11-11 16:26:30 +0200

    Common :: add code to SessionHolder to validate locale
    Supplementary changes to module that provides holder

--- app/src/main/java/com/techery/spares/session/SessionHolder.java
+++ app/src/main/java/com/techery/spares/session/SessionHolder.java
@@ -1,16 +1,42 @@
 package com.techery.spares.session;
 
+import com.innahema.collections.query.queriables.Queryable;
 import com.techery.spares.storage.complex_objects.ComplexObjectStorage;
 import com.techery.spares.storage.preferences.SimpleKeyValueStorage;
+import com.worldventures.dreamtrips.core.session.UserSession;
+import com.worldventures.dreamtrips.core.utils.LocaleHelper;
 
-public class SessionHolder<S> extends ComplexObjectStorage<S> {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+// generic here is for backward compatibility - to excape changing usage thoughout project
+// TODO :: 11.11.16 remove unused generic and it's usage
+public class SessionHolder<S> extends ComplexObjectStorage<UserSession> {
+
+   private final String[] supportedLanguageCodes = new String[] {"en-us", "el-cy", "el-gr", "es-us", "hu-hu", "ms-my",
+         "ro-hu", "sv-se", "zh", "zh-cn", "zh-hk", "zh-tw"};
+   private final List<Locale> supportedLocales;
+
+   @Inject public SessionHolder(SimpleKeyValueStorage keyValueStorage) {
+      super(keyValueStorage, "SESSION_KEY", UserSession.class);
+      supportedLocales = new ArrayList<>(Queryable.from(supportedLanguageCodes)
+            .map(LocaleHelper::buildFromLanguageCode).toList());
+   }
+
+   @Override
+   public void put(UserSession userSession) {
+      final Locale localeFromSession = LocaleHelper.buildFromLanguageCode(userSession.getLocale());
+      if (Queryable.from(supportedLocales).count(locale ->
+            LocaleHelper.compareLocales(localeFromSession, locale)) < 1) {
+         userSession.setLocale(Locale.US.getLanguage() + "-" + Locale.US.getCountry());
+      }
+      super.put(userSession);
+   }
 
    public interface Events {
       class SessionDestroyed {}
    }
-
-   public SessionHolder(SimpleKeyValueStorage keyValueStorage, Class<S> cls) {
-      super(keyValueStorage, "SESSION_KEY", cls);
-   }
-
 }

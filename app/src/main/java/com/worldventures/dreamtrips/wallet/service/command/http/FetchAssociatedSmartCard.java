6f71f03f31027cf096c4660a1758720574972321
Dmitry Sovgyr <dmitry.s@techery.io>
2016-11-22 17:02:26 +0200

    SmartCard :: Add compressing for smart card image after restoring cache from server

--- app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/http/FetchAssociatedSmartCard.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/http/FetchAssociatedSmartCard.java
@@ -5,14 +5,18 @@ import com.worldventures.dreamtrips.api.smart_card.association_info.model.SmartC
 import com.worldventures.dreamtrips.core.janet.JanetModule;
 import com.worldventures.dreamtrips.core.janet.dagger.InjectableAction;
 import com.worldventures.dreamtrips.core.repository.SnappyRepository;
+import com.worldventures.dreamtrips.wallet.domain.entity.ImmutableSmartCard;
 import com.worldventures.dreamtrips.wallet.domain.entity.SmartCard;
 import com.worldventures.dreamtrips.wallet.domain.entity.SmartCardDetails;
+import com.worldventures.dreamtrips.wallet.domain.entity.SmartCardUserPhoto;
 import com.worldventures.dreamtrips.wallet.service.SystemPropertiesProvider;
+import com.worldventures.dreamtrips.wallet.service.command.CompressImageForSmartCardCommand;
 import com.worldventures.dreamtrips.wallet.service.command.ConnectSmartCardCommand;
 
 import org.immutables.value.Value;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -43,7 +47,10 @@ public class FetchAssociatedSmartCard extends Command<FetchAssociatedSmartCard.A
       }
       janet.createPipe(GetAssociatedCardsHttpAction.class)
             .createObservableResult(new GetAssociatedCardsHttpAction(propertiesProvider.deviceId()))
-            .map(action -> handleResponse(action.response()))
+            .flatMap(action -> handleResponse(action.response()))
+            .doOnNext(result -> janetWallet.createPipe(ConnectSmartCardCommand.class)
+                  .send(new ConnectSmartCardCommand(result.smartCard(), true, true))
+            )
             .subscribe(callback::onSuccess, callback::onFail);
    }
 
@@ -53,19 +60,46 @@ public class FetchAssociatedSmartCard extends Command<FetchAssociatedSmartCard.A
       return snappyRepository.getSmartCard(activeSmartCardId);
    }
 
-   private FetchAssociatedSmartCard.AssociatedCard handleResponse(List<SmartCardInfo> listSmartCardInfo) {
-      if (listSmartCardInfo.isEmpty()) return ImmutableAssociatedCard.of(false);
-      SmartCardInfo smartCardInfo = listSmartCardInfo.get(0);
+   private Observable<FetchAssociatedSmartCard.AssociatedCard> handleResponse(List<SmartCardInfo> listSmartCardInfo) {
+      if (listSmartCardInfo.isEmpty()) return Observable.just(ImmutableAssociatedCard.of(false));
+      final SmartCardInfo smartCardInfo = listSmartCardInfo.get(0);
 
       final SmartCard smartCard = mappery.convert(smartCardInfo, SmartCard.class);
       final SmartCardDetails smartCardDetails = mappery.convert(smartCardInfo, SmartCardDetails.class);
 
+      return compressPhotoIfExist(smartCard)
+            .flatMap(sc -> save(sc, smartCardDetails));
+   }
+
+   private Observable<SmartCard> compressPhotoIfExist(SmartCard smartCard) {
+      final String photoUrl = smartCard.userPhoto();
+      if (photoUrl != null) {
+         return janetWallet.createPipe(CompressImageForSmartCardCommand.class)
+               .createObservableResult(new CompressImageForSmartCardCommand(photoUrl))
+               .map(command -> changeUserPhoto(smartCard, command.getResult()));
+      } else {
+         return Observable.just(smartCard);
+      }
+   }
+
+   private SmartCard changeUserPhoto(SmartCard smartCard, SmartCardUserPhoto smartCardUserPhoto) {
+      final File monochromeFile = smartCardUserPhoto.monochrome();
+      if (monochromeFile != null) {
+         return ImmutableSmartCard.builder()
+               .from(smartCard)
+               .userPhoto("file://" + monochromeFile.getAbsolutePath())
+               .build();
+      } else {
+         return smartCard;
+      }
+   }
+
+   private Observable<AssociatedCard> save(SmartCard smartCard, SmartCardDetails smartCardDetails) {
       snappyRepository.setActiveSmartCardId(smartCard.smartCardId());
       snappyRepository.saveSmartCard(smartCard);
       snappyRepository.saveSmartCardDetails(smartCardDetails);
 
-      janetWallet.createPipe(ConnectSmartCardCommand.class).send(new ConnectSmartCardCommand(smartCard, true, true));
-      return createAssociatedCard(smartCard, smartCardDetails);
+      return Observable.just(createAssociatedCard(smartCard, smartCardDetails));
    }
 
    private AssociatedCard createAssociatedCard(SmartCard smartCard, SmartCardDetails smartCardDetails) {

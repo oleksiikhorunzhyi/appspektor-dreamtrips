717ab2d68288ff1259d9727b7340a9da203e3572
Edward Fedorow <--global>
2016-11-28 16:45:41 +0200

    Change tab origin on fragments with tabs

--- app/src/main/java/com/worldventures/dreamtrips/modules/infopages/view/fragment/HelpFragment.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/infopages/view/fragment/HelpFragment.java
@@ -1,19 +1,42 @@
 package com.worldventures.dreamtrips.modules.infopages.view.fragment;
 
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.techery.spares.annotations.Layout;
+import com.techery.spares.annotations.MenuResource;
 import com.techery.spares.utils.delegate.ScreenChangedEventDelegate;
 import com.worldventures.dreamtrips.R;
 import com.worldventures.dreamtrips.core.navigation.Route;
+import com.worldventures.dreamtrips.modules.bucketlist.view.custom.CustomViewPager;
+import com.worldventures.dreamtrips.modules.common.presenter.Presenter;
+import com.worldventures.dreamtrips.modules.common.view.custom.BadgedTabLayout;
+import com.worldventures.dreamtrips.modules.common.view.fragment.BaseFragment;
 import com.worldventures.dreamtrips.modules.common.view.viewpager.BasePagerAdapter;
 import com.worldventures.dreamtrips.modules.common.view.viewpager.FragmentItem;
 
 import javax.inject.Inject;
 
-public class HelpFragment extends TermsTabFragment {
+import butterknife.InjectView;
+
+@Layout(R.layout.fragment_tab_info)
+@MenuResource(R.menu.menu_mock)
+public class HelpFragment extends BaseFragment<Presenter> {
+
+   protected static final int TERMS_OFFSCREEN_PAGES = 2;
+
+   @InjectView(R.id.tabs) protected BadgedTabLayout tabs;
+   @InjectView(R.id.pager) protected CustomViewPager pager;
 
    @Inject ScreenChangedEventDelegate screenChangedEventDelegate;
+
+   protected BasePagerAdapter adapter;
+
+   @Override
+   protected Presenter createPresenter(Bundle savedInstanceState) {
+      return new Presenter();
+   }
 
    @Override
    public void afterCreateView(View rootView) {

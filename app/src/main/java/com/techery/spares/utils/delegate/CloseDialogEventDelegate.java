9c81a5736fa20e76c0f2a551ebf30adaccc1ff3e
Alex Khorunzhyi <alex.kh@techery.io>
2016-12-14 14:10:28 +0200

    DTL :: add new delegates to event module

---
+++ app/src/main/java/com/techery/spares/utils/delegate/CloseDialogEventDelegate.java
@@ -0,0 +1,6 @@
+package com.techery.spares.utils.delegate;
+
+
+import com.worldventures.dreamtrips.modules.common.delegate.EventDelegate;
+
+public class CloseDialogEventDelegate extends EventDelegate<Object> {}

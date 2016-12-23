ac5a37ff52e2ac2fb822a8f41b4908319284205b
Edward Fedorow <palevomr@gmail.com>
2016-12-23 12:38:05 +0200

    Add at least dummy test for dtl (#2810)

---
+++ app/src/testDtl/java/com/worldventures/dreamtrips/dtl/DtlDummySpec.kt
@@ -0,0 +1,11 @@
+package com.worldventures.dreamtrips.social.trips
+
+import com.worldventures.dreamtrips.BaseSpec
+import kotlin.test.assertTrue
+
+class DtlDummySpec : BaseSpec({
+   //TODO we should not just remove tests!
+   describe("Dummy test") {
+      assertTrue { true }
+   }
+})

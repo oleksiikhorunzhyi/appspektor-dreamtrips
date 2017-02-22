16b019af853b98c7a76799218ac64d1f887175fe
Dmitry Sovgyr <dmitry.s@techery.io>
2017-02-22 18:07:33 +0200

    SmartCard :: remove delay from commect command

--- app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/ConnectSmartCardCommand.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/service/command/ConnectSmartCardCommand.java
@@ -3,8 +3,6 @@ package com.worldventures.dreamtrips.wallet.service.command;
 import com.worldventures.dreamtrips.core.janet.JanetModule;
 import com.worldventures.dreamtrips.core.janet.dagger.InjectableAction;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -13,7 +11,6 @@ import io.techery.janet.Janet;
 import io.techery.janet.command.annotations.CommandAction;
 import io.techery.janet.smartcard.action.support.ConnectAction;
 import io.techery.janet.smartcard.model.ImmutableConnectionParams;
-import rx.Observable;
 import timber.log.Timber;
 
 @CommandAction
@@ -22,28 +19,25 @@ public class ConnectSmartCardCommand extends Command<Void> implements Injectable
    @Inject @Named(JanetModule.JANET_WALLET) Janet janet;
 
    private final String smartCardId;
-   private final boolean waitForParing;
 
    public ConnectSmartCardCommand(String smartCardId, boolean waitForParing) {
       this.smartCardId = smartCardId;
-      this.waitForParing = waitForParing;
+   }
+
+   public ConnectSmartCardCommand(String smartCardId) {
+      this(smartCardId, false);
    }
 
    @Override
    @Deprecated
    protected void run(CommandCallback<Void> callback) throws Throwable {
-      Observable<ConnectAction> observable = janet.createPipe(ConnectAction.class)
-            .createObservableResult(new ConnectAction(ImmutableConnectionParams.of((int) Long.parseLong(smartCardId))));
-
-      if (waitForParing) {
-         observable = observable.delay(20L, TimeUnit.SECONDS); //TODO: Hard code for waiting typing PIN
-      }
-
-      observable.subscribe(value -> callback.onSuccess(null),
-            throwable -> {
-               Timber.e(throwable, "Error while connecting to smart card");
-               callback.onSuccess(null);
-            }
-      );
+      janet.createPipe(ConnectAction.class)
+            .createObservableResult(new ConnectAction(ImmutableConnectionParams.of((int) Long.parseLong(smartCardId))))
+            .subscribe(value -> callback.onSuccess(null),
+                  throwable -> {
+                     Timber.e(throwable, "Error while connecting to smart card");
+                     callback.onSuccess(null);
+                  }
+            );
    }
 }

5003ffa968f7a3c52719c1f3513782f18876ec20
Dmitry Sovgyr <dmitry.s@techery.io>
2017-03-06 09:53:41 +0200

    SMARTCARD-2347 Fix Card Manual Entered event

--- app/src/main/java/com/worldventures/dreamtrips/wallet/analytics/wizard/ScidEnteredAction.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/analytics/wizard/ScidEnteredAction.java
@@ -5,7 +5,7 @@ import com.worldventures.dreamtrips.core.utils.tracksystem.AnalyticsEvent;
 import com.worldventures.dreamtrips.core.utils.tracksystem.Attribute;
 import com.worldventures.dreamtrips.wallet.analytics.WalletAnalyticsAction;
 
-@AnalyticsEvent(action = "wallet:setup:Step 2:Card Successfully Entered",
+@AnalyticsEvent(action = "wallet:setup:Step 1:Card Successfully Entered",
                 trackers = AdobeTracker.TRACKER_KEY)
 public class ScidEnteredAction extends WalletAnalyticsAction {
 

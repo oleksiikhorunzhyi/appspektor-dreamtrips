31d38711163b1c568c65fd1955859360d4729d0e
Daniil P <daniil.pavenko@gmail.com>
2017-03-23 18:26:55 +0200

    SMARTCARD-2575 Wrong redirection if save billing address

--- app/src/main/java/com/worldventures/dreamtrips/wallet/ui/records/address/EditBillingAddressPresenter.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/records/address/EditBillingAddressPresenter.java
@@ -21,12 +21,9 @@ import com.worldventures.dreamtrips.wallet.ui.common.base.screen.WalletScreen;
 import com.worldventures.dreamtrips.wallet.ui.common.helper.ErrorHandler;
 import com.worldventures.dreamtrips.wallet.ui.common.helper.OperationActionStateSubscriberWrapper;
 import com.worldventures.dreamtrips.wallet.ui.common.navigation.Navigator;
-import com.worldventures.dreamtrips.wallet.ui.dashboard.CardListPath;
 import com.worldventures.dreamtrips.wallet.util.FormatException;
 
 import javax.inject.Inject;
-
-import flow.Flow;
 
 public class EditBillingAddressPresenter extends WalletPresenter<EditBillingAddressPresenter.Screen, Parcelable> {
 
@@ -70,7 +67,7 @@ public class EditBillingAddressPresenter extends WalletPresenter<EditBillingAddr
    }
 
    private void addressChanged() {
-      navigator.single(new CardListPath(), Flow.Direction.REPLACE);
+      navigator.goBack();
       analyticsInteractor.paycardAnalyticsCommandPipe()
             .send(new PaycardAnalyticsCommand(new BillingAddressSavedAction(), record));
    }

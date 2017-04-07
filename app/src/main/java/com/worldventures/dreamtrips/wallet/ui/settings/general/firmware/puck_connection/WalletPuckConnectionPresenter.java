436490c416cd159222752395b3cc507ff89bc226
Dmitry Reutov <dima.reutov@bkstg.com>
2017-04-07 19:22:42 +0300

    SMARTCARD-2721 Fixed UI on SC update screen

--- app/src/main/java/com/worldventures/dreamtrips/wallet/ui/settings/general/firmware/puck_connection/WalletPuckConnectionPresenter.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/ui/settings/general/firmware/puck_connection/WalletPuckConnectionPresenter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Parcelable;
 
 import com.techery.spares.module.Injector;
+import com.worldventures.dreamtrips.wallet.service.SmartCardInteractor;
+import com.worldventures.dreamtrips.wallet.service.command.SmartCardUserCommand;
 import com.worldventures.dreamtrips.wallet.ui.common.base.WalletPresenter;
 import com.worldventures.dreamtrips.wallet.ui.common.base.screen.WalletScreen;
 import com.worldventures.dreamtrips.wallet.ui.common.navigation.Navigator;
@@ -11,14 +13,30 @@ import com.worldventures.dreamtrips.wallet.ui.settings.general.firmware.download
 
 import javax.inject.Inject;
 
+import io.techery.janet.helper.ActionStateSubscriber;
+
 public class WalletPuckConnectionPresenter extends WalletPresenter<WalletPuckConnectionPresenter.Screen, Parcelable> {
 
    @Inject Navigator navigator;
+   @Inject SmartCardInteractor smartCardInteractor;
 
    public WalletPuckConnectionPresenter(Context context, Injector injector) {
       super(context, injector);
    }
 
+   @Override
+   public void onAttachedToWindow() {
+      super.onAttachedToWindow();
+      fetchUserPhoto();
+   }
+   private void fetchUserPhoto() {
+      smartCardInteractor.smartCardUserPipe()
+            .createObservable(SmartCardUserCommand.fetch())
+            .compose(bindViewIoToMainComposer())
+            .subscribe(new ActionStateSubscriber<SmartCardUserCommand>()
+                  .onSuccess(command -> getView().userPhoto(command.getResult().userPhoto().photoUrl()))
+            );
+   }
    void goNext() {
       navigator.withoutLast(new WalletDownloadFirmwarePath());
    }
@@ -28,7 +46,7 @@ public class WalletPuckConnectionPresenter extends WalletPresenter<WalletPuckCon
    }
 
    public interface Screen extends WalletScreen {
-
+      void userPhoto(String photoUrl);
    }
 
 }

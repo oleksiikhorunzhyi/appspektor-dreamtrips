8069b08fc89f3d26a640e105aee90bafc18b3a0b
Andres Rubiano <andres.rubiano@globant.com>
2017-05-17 17:57:35 +0000

    DTL-1183 Error after send a review

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/cell/DtlMerchantExpandableCell.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/cell/DtlMerchantExpandableCell.java
@@ -12,16 +12,18 @@ import android.widget.TextView;
 
 import com.innahema.collections.query.queriables.Queryable;
 import com.techery.spares.annotations.Layout;
+import com.techery.spares.session.SessionHolder;
 import com.techery.spares.ui.view.cell.AbstractDelegateCell;
 import com.trello.rxlifecycle.RxLifecycle;
 import com.worldventures.dreamtrips.R;
 import com.worldventures.dreamtrips.api.dtl.merchants.model.OfferType;
 import com.worldventures.dreamtrips.core.selectable.SelectableCell;
 import com.worldventures.dreamtrips.core.selectable.SelectableDelegate;
+import com.worldventures.dreamtrips.core.session.UserSession;
 import com.worldventures.dreamtrips.core.utils.DateTimeUtils;
 import com.worldventures.dreamtrips.core.utils.LocaleHelper;
 import com.worldventures.dreamtrips.core.utils.ViewUtils;
-import com.worldventures.dreamtrips.core.utils.tracksystem.AnalyticsInteractor;
+import com.worldventures.dreamtrips.modules.common.model.User;
 import com.worldventures.dreamtrips.modules.common.view.custom.ImageryDraweeView;
 import com.worldventures.dreamtrips.modules.dtl.helper.MerchantHelper;
 import com.worldventures.dreamtrips.modules.dtl.model.DistanceType;
@@ -29,9 +31,9 @@ import com.worldventures.dreamtrips.modules.dtl.model.merchant.ImmutableThinMerc
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.MerchantMedia;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.offer.Offer;
 import com.worldventures.dreamtrips.modules.dtl.model.merchant.operational_hour.OperationDay;
-import com.worldventures.dreamtrips.modules.dtl.model.merchant.reviews.ReviewSummary;
 import com.worldventures.dreamtrips.modules.dtl.view.cell.delegates.MerchantCellDelegate;
 import com.worldventures.dreamtrips.modules.dtl_flow.parts.merchants.ValidateReviewUtil;
+import com.worldventures.dreamtrips.modules.dtl_flow.parts.reviews.storage.ReviewStorage;
 
 import java.util.List;
 
@@ -60,13 +62,11 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
    @InjectView(R.id.ratingBarReviews) RatingBar mRatingBar;
    @InjectView(R.id.text_view_rating) TextView textViewRating;
 
-   @Inject AnalyticsInteractor analyticsInteractor;
-
    private SelectableDelegate selectableDelegate;
    private DistanceType distanceType;
    private boolean expanded;
 
-   private static final float DEFAULT_RATING_VALUE = 0;
+   @Inject SessionHolder<UserSession> appSessionHolder;
 
    public DtlMerchantExpandableCell(View view) {
       super(view);
@@ -76,7 +76,7 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
    protected void syncUIStateWithModel() {
       merchantName.setText(getModelObject().displayName());
       pricing.setRating(getModelObject().budget());
-      //
+
       setImage(getModelObject().images());
       setDistance();
       setCategories();
@@ -84,6 +84,7 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
       setOffersSection();
       setToggleView();
       setSelection(itemView);
+
       // TODO :: 5/20/16 Below is a dirtiest hack of all. If this is not re-done to proper
       // TODO :: expandable implementation until release or at most latest - until next release
       // TODO :: please tear off my hands
@@ -113,7 +114,8 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
 
    private void setDistance() {
       Resources res = merchantDistance.getResources();
-      ViewUtils.setTextOrHideView(merchantDistance, getModelObject().asMerchantAttributes().provideFormattedDistance(res, distanceType));
+      ViewUtils.setTextOrHideView(merchantDistance, getModelObject().asMerchantAttributes()
+            .provideFormattedDistance(res, distanceType));
    }
 
    private void setCategories() {
@@ -122,9 +124,11 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
    }
 
    private void setOperationalStatus() {
-      if (getModelObject().asMerchantAttributes().hasPoints() && getModelObject().asMerchantAttributes().hasOperationDays()) {
+      if (getModelObject().asMerchantAttributes().hasPoints() && getModelObject().asMerchantAttributes()
+            .hasOperationDays()) {
          ViewUtils.setViewVisibility(merchantOperationalStatus, View.VISIBLE);
-         Observable.fromCallable(() -> getModelObject().asMerchantAttributes().provideFormattedOperationalTime(itemView.getContext(), false))
+         Observable.fromCallable(() -> getModelObject().asMerchantAttributes()
+               .provideFormattedOperationalTime(itemView.getContext(), false))
                .compose(RxLifecycle.bindView(itemView))
                .subscribe(merchantOperationalStatus::setText, ex -> merchantOperationalStatus.setVisibility(View.GONE));
       } else ViewUtils.setViewVisibility(merchantOperationalStatus, View.INVISIBLE);
@@ -183,7 +187,8 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
    }
 
    private void bindPointsCell(Offer offer, ViewGroup container) {
-      View cellView = LayoutInflater.from(itemView.getContext()).inflate(R.layout.adapter_item_offer_points, container, false);
+      View cellView = LayoutInflater.from(itemView.getContext())
+            .inflate(R.layout.adapter_item_offer_points, container, false);
       cellView.setTag(offer);
       cellView.setOnClickListener(v -> cellDelegate.onOfferClick(getModelObject(), (Offer) v.getTag()));
       setSelection(cellView);
@@ -191,7 +196,8 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
    }
 
    private void bindPerkCell(Offer offer, ViewGroup container) {
-      View cellView = LayoutInflater.from(itemView.getContext()).inflate(R.layout.adapter_item_offer_perk, container, false);
+      View cellView = LayoutInflater.from(itemView.getContext())
+            .inflate(R.layout.adapter_item_offer_perk, container, false);
       cellView.setTag(offer);
       cellView.setOnClickListener(v -> cellDelegate.onOfferClick(getModelObject(), (Offer) v.getTag()));
       ImageryDraweeView image = ButterKnife.<ImageryDraweeView>findById(cellView, R.id.perk_logo);
@@ -227,10 +233,16 @@ public class DtlMerchantExpandableCell extends AbstractDelegateCell<ImmutableThi
 
    @OnClick(R.id.layout_rating_reviews)
    void onClickRateView() {
-      if (getModelObject().reviewSummary().userHasPendingReview() && Integer.parseInt(getModelObject().reviewSummary().total()) < 1) {
-         cellDelegate.userHasPendingReview();
+      User user = appSessionHolder.get().get().getUser();
+      if (!ReviewStorage.exists(itemView.getContext(), String.valueOf(user.getId()), getModelObject().id())) {
+         if (getModelObject().reviewSummary()
+               .userHasPendingReview() && Integer.parseInt(getModelObject().reviewSummary().total()) < 1) {
+            cellDelegate.userHasPendingReview();
+         } else {
+            cellDelegate.sendToRatingReview(getModelObject());
+         }
       } else {
-         cellDelegate.sendToRatingReview(getModelObject());
+         cellDelegate.userHasPendingReview();
       }
    }
 }

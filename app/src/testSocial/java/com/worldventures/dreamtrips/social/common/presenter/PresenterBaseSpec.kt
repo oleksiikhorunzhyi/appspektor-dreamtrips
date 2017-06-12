bcad5e83a20e11bbfec99e2c2f801e4e3ac4ead4
Roman Minenok <roman.m@techery.io>
2017-06-12 16:13:08 +0000

    SOCIAL-614 Add tests for documents lists presenters

--- app/src/testSocial/java/com/worldventures/dreamtrips/social/common/presenter/PresenterBaseSpec.kt
+++ app/src/testSocial/java/com/worldventures/dreamtrips/social/common/presenter/PresenterBaseSpec.kt
@@ -10,14 +10,16 @@ import com.worldventures.dreamtrips.core.api.PhotoUploadingManagerS3
 import com.worldventures.dreamtrips.core.navigation.ActivityRouter
 import com.worldventures.dreamtrips.core.session.acl.FeatureManager
 import com.worldventures.dreamtrips.core.utils.tracksystem.AnalyticsInteractor
+import com.worldventures.dreamtrips.modules.common.delegate.system.ConnectionInfoProvider
 import com.worldventures.dreamtrips.modules.common.presenter.delegate.OfflineWarningDelegate
 import de.greenrobot.event.EventBus
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.Spec
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
 @RunWith(JUnitPlatform::class)
-abstract class PresenterBaseSpec(spekBody: org.jetbrains.spek.api.dsl.Spec.() -> Unit) : Spek(spekBody) {
+abstract class PresenterBaseSpec(spekBody: Spec.() -> Unit) : Spek(spekBody) {
 
    companion object {
       init {
@@ -35,6 +37,7 @@ abstract class PresenterBaseSpec(spekBody: org.jetbrains.spek.api.dsl.Spec.() ->
             registerProvider(FeatureManager::class.java, { mock() })
             registerProvider(PhotoUploadingManagerS3::class.java, { mock() })
             registerProvider(OfflineWarningDelegate::class.java, { mock() })
+            registerProvider(ConnectionInfoProvider::class.java, { mock() })
          }
       }
    }

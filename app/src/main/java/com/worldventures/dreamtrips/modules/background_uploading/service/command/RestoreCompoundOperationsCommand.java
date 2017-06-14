da37b2b6c9d4eb1cb9a7a83391cd4acec74ea728
Roman Minenok <roman.m@techery.io>
2017-06-14 13:27:40 +0300

    Fix bugs with restoring compound operations commands

--- app/src/main/java/com/worldventures/dreamtrips/modules/background_uploading/service/command/RestoreCompoundOperationsCommand.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/background_uploading/service/command/RestoreCompoundOperationsCommand.java
@@ -28,11 +28,16 @@ public class RestoreCompoundOperationsCommand extends Command<List<PostCompoundO
                List<PostCompoundOperationModel> cachedModels = command.getResult();
                for (int i = 0; i < cachedModels.size(); i++) {
                   PostCompoundOperationModel item = cachedModels.get(i);
-                  if (item instanceof PostCompoundOperationModel &&
-                        cachedModels.get(i).state() == CompoundOperationState.STARTED) {
-                     cachedModels.remove(i);
-                     cachedModels.add(i, ImmutablePostCompoundOperationModel.copyOf((PostCompoundOperationModel) item)
-                           .withState(CompoundOperationState.PAUSED));
+                  CompoundOperationState state = item.state();
+                  switch (state) {
+                     case STARTED:
+                        cachedModels.remove(i);
+                        cachedModels.add(i, ImmutablePostCompoundOperationModel.copyOf((PostCompoundOperationModel) item)
+                              .withState(CompoundOperationState.PAUSED));
+                        break;
+                     case FINISHED:
+                        cachedModels.remove(i);
+                        break;
                   }
                }
                compoundOperationsInteractor.compoundOperationsPipe().send(CompoundOperationsCommand.compoundOperationsChanged(cachedModels));

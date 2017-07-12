c54ef4b2d6842e3533915417c5c93fcc9b5eac1c
Diego Marcher <diego.marcher@globant.com>
2017-07-12 15:48:57 -0300

    Javascript callback handlers added

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/custom/webview/javascript/JavaScriptInterface.java
@@ -0,0 +1,20 @@
+package com.worldventures.dreamtrips.modules.dtl.view.custom.webview.javascript;
+
+import android.webkit.JavascriptInterface;
+
+public abstract class JavaScriptInterface {
+   public static final String JAVASCRIPT_INTERFACE = "mobileTHRSTContext";
+
+   @JavascriptInterface
+   @SuppressWarnings("unused")
+   public void sendMessage(String message) {
+      thrstCallback(message);
+   }
+
+   public abstract void thrstCallback(String message);
+
+   @Override
+   public String toString() {
+      return JAVASCRIPT_INTERFACE;
+   }
+}

c7ab24bcf49011571da833ecb9830d923ff7f5aa
Dmitry Sovgyr <dmitry.s@techery.io>
2017-07-21 14:42:01 +0300

    SMARTCARD-3475 Remove ReactiveLocationProvider from wallet part

--- app/src/main/java/com/worldventures/dreamtrips/wallet/service/impl/AndroidDetectLocationService.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/service/impl/AndroidDetectLocationService.java
@@ -10,27 +10,34 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.content.PermissionChecker;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.worldventures.dreamtrips.wallet.service.WalletDetectLocationService;
 import com.worldventures.dreamtrips.wallet.util.LocationUnavailableException;
 
-import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
 import rx.Observable;
 import rx.Subscriber;
+import rx.subscriptions.Subscriptions;
+import timber.log.Timber;
+
+import static com.google.android.gms.location.LocationServices.FusedLocationApi;
+import static rx.Observable.create;
+import static rx.Observable.fromCallable;
 
 public class AndroidDetectLocationService implements WalletDetectLocationService {
 
    private final Context context;
-   private final ReactiveLocationProvider locationProvider;
    private final LocationManager locationManager;
 
    public AndroidDetectLocationService(Context context) {
       this.context = context;
-      this.locationProvider = new ReactiveLocationProvider(context);
       this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
    }
 
@@ -56,25 +63,29 @@ public class AndroidDetectLocationService implements WalletDetectLocationService
 
    @Override
    public Observable<Location> detectLastKnownLocation() {
-      return locationProvider.getLastKnownLocation().switchIfEmpty(requestUpdatedLocation());
+      return connectObservable().flatMap(googleApiClient ->
+            fromCallable(() -> FusedLocationApi.getLastLocation(googleApiClient))
+                  .filter(location -> location != null)
+                  .switchIfEmpty(requestUpdatedLocation(googleApiClient).take(1))
+      );
    }
 
-   private Observable<Location> requestUpdatedLocation() {
-      return checkSettings().flatMap(this::settingsResultObtained);
-   }
-
-   private Observable<LocationSettingsResult> checkSettings() {
-      return locationProvider.checkLocationSettings(new LocationSettingsRequest.Builder().addLocationRequest(provideLocationRequest())
-            .setAlwaysShow(true)
-            .build());
-   }
-
-   private Observable<Location> settingsResultObtained(LocationSettingsResult locationSettingsResult) {
-      Status status = locationSettingsResult.getStatus();
+   private Observable<Location> requestUpdatedLocation(GoogleApiClient googleApiClient) {
+      final LocationSettingsResult locationSettingsResult = checkSettings(googleApiClient);
+      final Status status = locationSettingsResult.getStatus();
       if (status.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
-         return Observable.error(new LocationUnavailableException());
+         return Observable.error(new LocationUnavailableException(locationSettingsResult));
       }
-      return locationProvider.getUpdatedLocation(provideLocationRequest());
+
+      return Observable.create(new RxFusedLocationApi(googleApiClient, provideLocationRequest()));
+   }
+
+   private LocationSettingsResult checkSettings(GoogleApiClient googleApiClient) {
+      return LocationServices.SettingsApi.checkLocationSettings(googleApiClient, new LocationSettingsRequest.Builder()
+            .addLocationRequest(provideLocationRequest())
+            .setAlwaysShow(true)
+            .build())
+            .await();
    }
 
    private LocationRequest provideLocationRequest() {
@@ -88,7 +99,7 @@ public class AndroidDetectLocationService implements WalletDetectLocationService
       private final Context appContext;
       private BroadcastReceiver broadcastReceiver;
 
-      public RxLocationAdapter(Context appContext) {
+      RxLocationAdapter(Context appContext) {
          this.appContext = appContext;
       }
 
@@ -105,6 +116,59 @@ public class AndroidDetectLocationService implements WalletDetectLocationService
 
       public void release() {
          appContext.unregisterReceiver(broadcastReceiver);
+      }
+   }
+
+   private static class RxFusedLocationApi implements Observable.OnSubscribe<Location> {
+
+      private final GoogleApiClient googleApiClient;
+      private final LocationRequest locationRequest;
+
+      private RxFusedLocationApi(GoogleApiClient googleApiClient, LocationRequest locationRequest) {
+         this.googleApiClient = googleApiClient;
+         this.locationRequest = locationRequest;
+      }
+
+      @Override
+      public void call(Subscriber<? super Location> subscriber) {
+         LocationListener listener = subscriber::onNext;
+         FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, listener); // TODO: 7/20/17 use looper
+         subscriber.add(Subscriptions.create(() -> FusedLocationApi.removeLocationUpdates(googleApiClient, listener)));
+      }
+   }
+
+   private Observable<GoogleApiClient> connectObservable() {
+      return create(subscriber -> {
+         final ClientWithStatus clientWithStatus = createAndConnect();
+         if (clientWithStatus.connectionResult.isSuccess()) {
+            subscriber.onNext(clientWithStatus.googleApiClient);
+         }
+         subscriber.onCompleted();
+         subscriber.add(Subscriptions.create(() -> {
+            clientWithStatus.googleApiClient.disconnect();
+            Timber.d("connectObservable googleApiClient.disconnect Tread %s", Thread.currentThread());
+         }));
+      });
+   }
+
+   private ClientWithStatus createAndConnect() {
+      final GoogleApiClient client = createLocationApiClient();
+      return new ClientWithStatus(client, client.blockingConnect());
+   }
+
+   private GoogleApiClient createLocationApiClient() {
+      return new GoogleApiClient.Builder(context)
+            .addApi(LocationServices.API)
+            .build();
+   }
+
+   private static class ClientWithStatus {
+      final GoogleApiClient googleApiClient;
+      final ConnectionResult connectionResult;
+
+      private ClientWithStatus(GoogleApiClient googleApiClient, ConnectionResult connectionResult) {
+         this.googleApiClient = googleApiClient;
+         this.connectionResult = connectionResult;
       }
    }
 }

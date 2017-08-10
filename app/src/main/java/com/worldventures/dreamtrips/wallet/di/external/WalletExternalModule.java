df830aab7128d5ed3e410363c6fb49c835f876d7
Dmitry Sovgyr <dmitry.s@techery.io>
2017-08-10 17:05:28 +0300

    Move wallet external implementation to separate package

--- app/src/main/java/com/worldventures/dreamtrips/wallet/di/external/WalletExternalModule.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/di/external/WalletExternalModule.java
@@ -3,7 +3,10 @@ package com.worldventures.dreamtrips.wallet.di.external;
 import com.techery.spares.session.SessionHolder;
 import com.worldventures.dreamtrips.core.session.UserSession;
 import com.worldventures.dreamtrips.modules.video.utils.CachedModelHelper;
+import com.worldventures.dreamtrips.wallet.service.WalletSocialInfoProvider;
 import com.worldventures.dreamtrips.wallet.ui.settings.help.video.holder.WalletVideoHolderDelegate;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -14,5 +17,11 @@ public class WalletExternalModule {
    @Provides
    WalletVideoHolderDelegate provideVideoHolderDelegate(SessionHolder<UserSession> appSessionHolder, CachedModelHelper cachedModelHelper) {
       return new WalletVideoHolderDelegateImpl(appSessionHolder, cachedModelHelper);
+   }
+
+   @Singleton
+   @Provides
+   WalletSocialInfoProvider walletSocialInfoProvider(SessionHolder<UserSession> sessionHolder) {
+      return new WalletSocialInfoProviderImpl(sessionHolder);
    }
 }

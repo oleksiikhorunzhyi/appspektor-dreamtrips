5c6d996ab400fcbc00bf3284fc137c2af5d9ce33
Daniel Pavenko <daniel.pavenko@techery.io>
2017-10-04 12:41:04 +0300

    COMMON disable SmartCard features for release

--- app/src/main/java/com/worldventures/dreamtrips/wallet/di/SmartCardModule.java
+++ app/src/main/java/com/worldventures/dreamtrips/wallet/di/SmartCardModule.java
@@ -16,7 +16,7 @@ import com.worldventures.dreamtrips.wallet.domain.storage.action.PersistentDevic
 import com.worldventures.dreamtrips.wallet.service.RecordInteractor;
 import com.worldventures.dreamtrips.wallet.service.logout.WalletLogoutActionModule;
 import com.worldventures.dreamtrips.wallet.util.WalletFeatureHelper;
-import com.worldventures.dreamtrips.wallet.util.WalletFeatureHelperFull;
+import com.worldventures.dreamtrips.wallet.util.WalletFeatureHelperRelease;
 
 import javax.inject.Named;
 import javax.inject.Provider;
@@ -78,8 +78,8 @@ public class SmartCardModule {
    @Singleton
    @Provides
    WalletFeatureHelper featureHelper(@Named(JANET_WALLET) Janet janet, RecordInteractor recordInteractor) {
-      //      return new WalletFeatureHelperRelease(janet, recordInteractor);
-      return new WalletFeatureHelperFull();
+      return new WalletFeatureHelperRelease(janet, recordInteractor);
+      //return new WalletFeatureHelperFull();
    }
 
    @Provides

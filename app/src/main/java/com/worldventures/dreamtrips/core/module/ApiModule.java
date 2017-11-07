c9d489bef3bbc3a1792d385c339622d7df386cd8
Edward Fedorov <edward.f@techery.io>
2017-11-07 15:02:23 +0000

    SOCIAL-1480 Clean up modules from unused fields

--- app/src/main/java/com/worldventures/dreamtrips/core/module/ApiModule.java
+++ app/src/main/java/com/worldventures/dreamtrips/core/module/ApiModule.java
@@ -6,22 +6,11 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.techery.spares.utils.gson.LowercaseEnumTypeAdapterFactory;
-import com.worldventures.core.modules.settings.model.Setting;
-import com.worldventures.core.modules.settings.model.serializer.SettingsDeserializer;
-import com.worldventures.core.modules.settings.model.serializer.SettingsSerializer;
 import com.worldventures.core.utils.AppVersionNameBuilder;
 import com.worldventures.core.utils.DateTimeDeserializer;
 import com.worldventures.core.utils.DateTimeSerializer;
 import com.worldventures.dreamtrips.core.utils.HeaderProvider;
 import com.worldventures.dreamtrips.core.utils.PersistentCookieStore;
-import com.worldventures.dreamtrips.social.ui.bucketlist.service.model.GsonAdaptersBucketBodyImpl;
-import com.worldventures.dreamtrips.social.ui.bucketlist.service.model.GsonAdaptersBucketCoverBody;
-import com.worldventures.dreamtrips.social.ui.bucketlist.service.model.GsonAdaptersBucketPostBody;
-import com.worldventures.dreamtrips.social.ui.bucketlist.service.model.GsonAdaptersBucketStatusBody;
-import com.worldventures.dreamtrips.social.ui.feed.model.FeedEntityHolder;
-import com.worldventures.dreamtrips.social.ui.feed.model.FeedItem;
-import com.worldventures.dreamtrips.social.ui.feed.model.serializer.FeedEntityDeserializer;
-import com.worldventures.dreamtrips.social.ui.feed.model.serializer.FeedItemDeserializer;
 
 import java.net.CookieManager;
 import java.net.CookiePolicy;
@@ -40,15 +29,6 @@ public class ApiModule {
             .registerTypeAdapterFactory(new LowercaseEnumTypeAdapterFactory("unknown"))
             .registerTypeAdapter(Date.class, new DateTimeDeserializer())
             .registerTypeAdapter(Date.class, new DateTimeSerializer())
-            .registerTypeAdapter(FeedItem.class, new FeedItemDeserializer())
-            .registerTypeAdapter(FeedEntityHolder.class, new FeedEntityDeserializer())
-            .registerTypeAdapter(Setting.class, new SettingsDeserializer())
-            .registerTypeAdapter(Setting.class, new SettingsSerializer())
-            //new
-            .registerTypeAdapterFactory(new GsonAdaptersBucketPostBody())
-            .registerTypeAdapterFactory(new GsonAdaptersBucketCoverBody())
-            .registerTypeAdapterFactory(new GsonAdaptersBucketStatusBody())
-            .registerTypeAdapterFactory(new GsonAdaptersBucketBodyImpl())
             .create();
    }
 

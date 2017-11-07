4ad568909e472adc5eeafebbeddd798945034b09
Edward Fedorov <edward.f@techery.io>
2017-11-07 18:01:04 +0200

    SOCIAL-1705 fix issue with points

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/fragment/DtlThrstFlowFragment.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl/view/fragment/DtlThrstFlowFragment.java
@@ -84,7 +84,7 @@ public class DtlThrstFlowFragment extends RxBaseFragmentWithArgs<DtlThrstFlowPre
                   .backStackEnabled(true)
                   .clearBackStack(true)
                   .data(new ThrstPaymentBundle(getArgs().getMerchant(), isPaid, response.billTotal(),
-                        response.pointsAmount(), response.pointsAmount(), response.billImagePath(), response.subTotal(),
+                        response.pointsAmount(), response.totalPoints(), response.billImagePath(), response.subTotal(),
                         response.tax(), response.tip()))
                   .build()
       );

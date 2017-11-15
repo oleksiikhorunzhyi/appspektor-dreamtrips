e03787fae1d357b45d9b6e291101b6c1f0c5a6eb
Rafael Vazquez Andraca <rafael.vazquez@globant.com>
2017-11-15 11:42:44 -0600

    REMOVED > Icon status from Non Thrst Transactions

--- app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/transactions/adapter/SearchableTransactionsAdapter.java
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/transactions/adapter/SearchableTransactionsAdapter.java
@@ -82,7 +82,10 @@ public class SearchableTransactionsAdapter extends RecyclerView.Adapter<Recycler
          earnedPoints.setText(getEarnedPointText(transactionModel.getEarnedPoints()));
          earnedPointsIcon.setVisibility(View.VISIBLE);
          earnedPointsIcon.setBackgroundResource(R.drawable.dt_points_big_icon);
-         statusImageView.setImageResource(TransactionModel.ThrstPaymentStatus.SUCCESSFUL.equals(transactionModel.getThrstPaymentStatus()) ? R.drawable.check_succes_pilot : R.drawable.check_error_pilot);
+         if(transactionModel.isTrhstTransaction())
+            statusImageView.setImageResource(TransactionModel.ThrstPaymentStatus.SUCCESSFUL.equals(transactionModel.getThrstPaymentStatus()) ? R.drawable.check_succes_pilot : R.drawable.check_error_pilot);
+         else
+            statusImageView.setImageBitmap(null);
          transactionDate.setText(DateTimeUtils.convertDateToString(transactionModel.getTransactionDate(),
                DateTimeUtils.TRANSACTION_DATE_FORMAT));
       }

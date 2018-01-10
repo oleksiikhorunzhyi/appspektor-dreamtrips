dbe39734ccfca9a8c58d2334cc2cb0fe577c8d0f
Alex Khorunzhy <alex.kh@techery.io>
2018-01-10 17:28:12 +0200

    DTL :: new error mapping

---
+++ app/src/main/java/com/worldventures/dreamtrips/modules/dtl_flow/parts/review/PostReviewError.java
@@ -0,0 +1,35 @@
+package com.worldventures.dreamtrips.modules.dtl_flow.parts.review;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
+
+public enum PostReviewError {
+
+   PROFANITY("ERROR_FORM_PROFANITY"),
+   UNKNOWN("ERROR_UNKNOWN"),
+   REQUESTS_LIMIT("ERROR_REQUEST_LIMIT_REACHED"),
+   DUPLICATED("ERROR_DUPLICATED_REVIEW"),
+   UNRECOGNIZED("");
+
+   private final String error;
+
+   PostReviewError(String error) {
+      this.error = error;
+   }
+
+   public String error() {
+      return error;
+   }
+
+   public static PostReviewError of(@Nullable String error) {
+      if (error == null) return UNRECOGNIZED;
+
+      for (PostReviewError name : values()) {
+         if (String.format(name.error(), Locale.ENGLISH).equalsIgnoreCase(error)) {
+            return name;
+         }
+      }
+      return UNRECOGNIZED;
+   }
+}

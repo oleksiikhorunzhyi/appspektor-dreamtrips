b553957a235300691d3383289df2e2286514c01c
Roman Minenok <roman.m@techery.io>
2018-02-02 13:33:05 +0200

    Fix cropped countries list in videos section

--- wallet/src/main/java/com/worldventures/wallet/ui/settings/help/video/impl/language/HelpVideoLanguagePicker.kt
+++ wallet/src/main/java/com/worldventures/wallet/ui/settings/help/video/impl/language/HelpVideoLanguagePicker.kt
@@ -5,12 +5,15 @@ import android.view.View
 import android.widget.ListPopupWindow
 import com.afollestad.materialdialogs.MaterialDialog
 import com.worldventures.core.modules.video.model.VideoLocale
+import com.worldventures.core.ui.util.ViewUtils
 import com.worldventures.wallet.R
 import com.worldventures.wallet.ui.settings.help.video.impl.HelpVideoLocale
 import com.worldventures.wallet.ui.settings.help.video.impl.language.adapter.VideoLanguagesAdapter
 import com.worldventures.wallet.ui.settings.help.video.impl.language.adapter.VideoLocaleAdapter
 
 private typealias LocaleCallback = (HelpVideoLocale) -> Unit
+
+private const val POPUP_WINDOW_WIDTH_DP = 200f
 
 class HelpVideoLanguagePicker(
       private val context: Context,
@@ -41,6 +44,8 @@ class HelpVideoLanguagePicker(
          videoLanguageSelected(locales[position])
          popup.dismiss()
       }
+
+      popup.width = ViewUtils.pxFromDp(anchor.context, POPUP_WINDOW_WIDTH_DP).toInt()
       popup.isModal = true
       popup.anchorView = anchor
       popup.show()
